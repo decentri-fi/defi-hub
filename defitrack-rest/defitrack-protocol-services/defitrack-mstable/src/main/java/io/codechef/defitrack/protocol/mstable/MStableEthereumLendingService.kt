f09801b81f5d1a2c53bcbf702f2447de8bf665af
TheMoneyLisa <moneylisa@defitrack.io>
2021-11-12 17:53:41 +0100

    Added mstable

---
+++ defitrack-rest/defitrack-protocol-services/defitrack-mstable/src/main/java/io/codechef/defitrack/protocol/mstable/MStableEthereumLendingService.kt
@@ -0,0 +1,59 @@
+package io.codechef.defitrack.protocol.mstable
+
+import io.codechef.common.network.Network
+import io.codechef.defitrack.abi.ABIResource
+import io.codechef.defitrack.lending.domain.LendingElement
+import io.codechef.defitrack.lending.LendingService
+import io.codechef.ethereum.config.EthereumContractAccessor
+import io.codechef.protocol.Protocol
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.RoundingMode
+import java.util.*
+
+@Service
+class MStableEthereumLendingService(
+    private val mStableService: MStableEthereumService,
+    private val abiResource: ABIResource,
+    private val ethereumContractAccessor: EthereumContractAccessor,
+) : LendingService {
+
+    val savingsContractABI by lazy {
+        abiResource.getABI("mStable/SavingsContract.json")
+    }
+
+    override fun getProtocol(): Protocol {
+        return Protocol.MSTABLE
+    }
+
+    override fun getNetwork(): Network {
+        return Network.ETHEREUM
+    }
+
+    override fun getLendings(address: String): List<LendingElement> =
+        mStableService.getSavingsContracts().map {
+            MStableEthereumSavingsContract(
+                ethereumContractAccessor,
+                savingsContractABI,
+                it
+            )
+        }.mapNotNull {
+            val balance = it.balanceOf(address)
+            if (balance > BigInteger.ZERO) {
+                LendingElement(
+                    user = address,
+                    id = UUID.randomUUID().toString(),
+                    network = getNetwork(),
+                    protocol = getProtocol(),
+                    name = it.name,
+                    amount = balance.toBigDecimal().divide(
+                        BigDecimal.TEN.pow(it.decimals), 6, RoundingMode.HALF_UP
+                    ).toPlainString(),
+                    symbol = it.symbol,
+                )
+            } else {
+                null
+            }
+        }
+}
---
+++ defitrack-rest/defitrack-protocol-services/defitrack-mstable/src/main/java/io/codechef/defitrack/protocol/mstable/MStableEthereumStakingService.kt
@@ -0,0 +1,79 @@
+package io.codechef.defitrack.protocol.mstable
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.codechef.common.network.Network
+import io.codechef.defitrack.abi.ABIResource
+import io.codechef.defitrack.staking.UserStakingService
+import io.codechef.defitrack.staking.domain.StakingElement
+import io.codechef.defitrack.staking.domain.VaultRewardToken
+import io.codechef.defitrack.token.TokenService
+import io.codechef.ethereum.config.EthereumContractAccessor
+import io.codechef.protocol.Protocol
+import org.springframework.stereotype.Service
+import java.math.BigInteger
+import java.util.*
+
+@Service
+class MStableEthereumStakingService(
+    private val mStableEthereumService: MStableEthereumService,
+    private val ethereumContractAccessor: EthereumContractAccessor,
+    private val abiResource: ABIResource,
+    tokenService: TokenService,
+    objectMapper: ObjectMapper
+) : UserStakingService(tokenService, objectMapper) {
+
+    val boostedSavingsVaultABI by lazy {
+        abiResource.getABI("mStable/BoostedSavingsVault.json")
+    }
+
+    override fun getStakings(address: String): List<StakingElement> {
+
+        val vaultContracts = mStableEthereumService.getBoostedSavingsVaults().map {
+            MStableEthereumBoostedSavingsVaultContract(
+                ethereumContractAccessor,
+                boostedSavingsVaultABI,
+                it
+            )
+        }
+
+        return vaultContracts.mapNotNull {
+            val balance = it.rawBalanceOf(address)
+
+            if (balance > BigInteger.ZERO) {
+                val stakingToken = tokenService.getTokenInformation(it.stakingToken, getNetwork())
+                val rewardsToken = tokenService.getTokenInformation(it.rewardsToken, getNetwork())
+                StakingElement(
+                    id = UUID.randomUUID().toString(),
+                    network = this.getNetwork(),
+                    protocol = this.getProtocol(),
+                    name = it.name,
+                    url = "https://etherscan.io/address/${it.address}",
+                    stakedToken = vaultStakedToken(
+                        stakingToken.address,
+                        balance
+                    ),
+                    rewardTokens = listOf(
+                        VaultRewardToken(
+                            name = rewardsToken.name,
+                            symbol = stakingToken.symbol,
+                            url = "https://etherscan.io/address/${stakingToken.address}",
+                        )
+                    ),
+                    contractAddress = it.address,
+                    vaultType = "mstable-boosted-savings-vault"
+                )
+            } else {
+                null
+            }
+        }
+    }
+
+    override fun getProtocol(): Protocol {
+        return Protocol.MSTABLE
+    }
+
+    override fun getNetwork(): Network {
+        return Network.ETHEREUM
+    }
+
+}
