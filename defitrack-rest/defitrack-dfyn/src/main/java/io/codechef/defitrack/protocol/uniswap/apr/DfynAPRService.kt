70eb06dd9e71366c5c587c50d41f2b2965a7c8d2
TheMoneyLisa <moneylisa@defitrack.io>
2021-11-12 17:50:40 +0100

    Added dfyn

---
+++ defitrack-rest/defitrack-dfyn/src/main/java/io/codechef/defitrack/protocol/uniswap/apr/DfynAPRService.kt
@@ -0,0 +1,35 @@
+package io.codechef.defitrack.protocol.uniswap.apr
+
+import io.codechef.protocol.dfyn.DfynService
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Component
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+@Component
+class DfynAPRService(private val dfynService: DfynService) {
+
+    @Cacheable(cacheNames = ["dfyn-aprs"], key = "#address")
+    fun getAPR(address: String): BigDecimal {
+        try {
+            val pairData = dfynService.getPairDayData(address)
+            return if (pairData.size <= 1) {
+                BigDecimal.ZERO
+            } else {
+                pairData.drop(1).map {
+                    it.dailyVolumeUSD
+                }.reduce { a, b -> a.plus(b) }
+                    .times(BigDecimal.valueOf(0.003)).times(BigDecimal.valueOf(52))
+                    .divide(
+                        dfynService.getPairs().find {
+                            it.id == address
+                        }!!.reserveUSD,
+                        18,
+                        RoundingMode.HALF_UP
+                    )
+            }
+        } catch (ex: Exception) {
+            return BigDecimal.ZERO
+        }
+    }
+}
