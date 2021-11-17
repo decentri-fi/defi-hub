5b7d4811204d2ffa37556fb9b8b8a0f08c413dc9
TheMoneyLisa <moneylisa@defitrack.io>
2021-11-17 15:57:10 +0100

    Fix package

--- ci/package.sh
+++ ci/package.sh
@@ -1,70 +1,55 @@
 #!/bin/bash
 
-docker build -t defitrack/defitrack:website-${BRANCH_NAME} defitrack/defitrack-web
-docker push defitrack/defitrack:website-${BRANCH_NAME}
-
-docker build -t defitrack/defitrack:uniswap-${BRANCH_NAME} defitrack/defitrack-uniswap
+docker build -t defitrack/defitrack:uniswap-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-uniswap
 docker push defitrack/defitrack:uniswap-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:quickswap-${BRANCH_NAME} defitrack/defitrack-quickswap
+docker build -t defitrack/defitrack:quickswap-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-quickswap
 docker push defitrack/defitrack:quickswap-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:sushiswap-${BRANCH_NAME} defitrack/defitrack-sushiswap
+docker build -t defitrack/defitrack:sushiswap-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-sushiswap
 docker push defitrack/defitrack:sushiswap-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:adamant-${BRANCH_NAME} defitrack/defitrack-adamant
+docker build -t defitrack/defitrack:adamant-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-adamant
 docker push defitrack/defitrack:adamant-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:api-gw-${BRANCH_NAME} defitrack/defitrack-api-gw
+docker build -t defitrack/defitrack:api-gw-${BRANCH_NAME} defitrack-rest/defitrack-gw
 docker push defitrack/defitrack:api-gw-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:dfyn-${BRANCH_NAME} defitrack/defitrack-dfyn
+docker build -t defitrack/defitrack:dfyn-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-dfyn
 docker push defitrack/defitrack:dfyn-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:balancer-${BRANCH_NAME} defitrack/defitrack-balancer
+docker build -t defitrack/defitrack:balancer-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-balancer
 docker push defitrack/defitrack:balancer-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:dmm-${BRANCH_NAME} defitrack/defitrack-dmm
+docker build -t defitrack/defitrack:dmm-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-dmm
 docker push defitrack/defitrack:dmm-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:mstable-${BRANCH_NAME} defitrack/defitrack-mstable
+docker build -t defitrack/defitrack:mstable-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-mstable
 docker push defitrack/defitrack:mstable-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:aave-${BRANCH_NAME} defitrack/defitrack-aave
+docker build -t defitrack/defitrack:aave-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-aave
 docker push defitrack/defitrack:aave-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:beefy-${BRANCH_NAME} defitrack/defitrack-beefy
+docker build -t defitrack/defitrack:beefy-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-beefy
 docker push defitrack/defitrack:beefy-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:curve-${BRANCH_NAME} defitrack/defitrack-curve
+docker build -t defitrack/defitrack:curve-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-curve
 docker push defitrack/defitrack:curve-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:dracula-${BRANCH_NAME} defitrack/defitrack-dracula
-docker push defitrack/defitrack:dracula-${BRANCH_NAME}
-
-docker build -t defitrack/defitrack:belt-${BRANCH_NAME} defitrack/defitrack-belt
-docker push defitrack/defitrack:belt-${BRANCH_NAME}
-
-docker build -t defitrack/defitrack:iron-${BRANCH_NAME} defitrack/defitrack-iron
-docker push defitrack/defitrack:iron-${BRANCH_NAME}
-
-docker build -t defitrack/defitrack:polycat-${BRANCH_NAME} defitrack/defitrack-polycat
+docker build -t defitrack/defitrack:polycat-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-polycat
 docker push defitrack/defitrack:polycat-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:yearn-${BRANCH_NAME} defitrack/defitrack-yearn
-docker push defitrack/defitrack:yearn-${BRANCH_NAME}
-
-docker build -t defitrack/defitrack:convex-${BRANCH_NAME} defitrack/defitrack-convex
+docker build -t defitrack/defitrack:convex-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-convex
 docker push defitrack/defitrack:convex-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:compound-${BRANCH_NAME} defitrack/defitrack-compound
+docker build -t defitrack/defitrack:compound-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-compound
 docker push defitrack/defitrack:compound-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:price-${BRANCH_NAME} defitrack/defitrack-price
+docker build -t defitrack/defitrack:price-${BRANCH_NAME} defitrack-rest/defitrack-uniswap
 docker push defitrack/defitrack:price-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:abi-${BRANCH_NAME} defitrack/defitrack-abi
+docker build -t defitrack/defitrack:abi-${BRANCH_NAME} defitrack-rest/defitrack-abi
 docker push defitrack/defitrack:abi-${BRANCH_NAME}
 
-docker build -t defitrack/defitrack:erc20-${BRANCH_NAME} defitrack/defitrack-erc20
+docker build -t defitrack/defitrack:erc20-${BRANCH_NAME} defitrack-rest/defitrack-erc20
 docker push defitrack/defitrack:erc20-${BRANCH_NAME}
