9372f45b7cb1917a17694189257383fc916c2279
TheMoneyLisa <moneylisa@defitrack.io>
2021-11-30 15:51:19 +0100

    Dinoswap

---
+++ defitrack-protocols/dinoswap/src/main/java/io/defitrack/protocol/dinoswap/DinoswapService.kt
@@ -0,0 +1,8 @@
+package io.defitrack.protocol.dinoswap
+
+import org.springframework.stereotype.Service
+
+@Service
+class DinoswapService {
+    fun getDinoFossilFarms(): List<String> = listOf("0x1948abc5400aa1d72223882958da3bec643fb4e5")
+}
