be1e9fb5a0d2bf087ecf84015b2e3e15b60870de
TheMoneyLisa <moneylisa@defitrack.io>
2021-12-03 16:23:35 +0100

    balance config

---
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/balance/BalanceRouterConfig.kt
@@ -0,0 +1,27 @@
+package io.defitrack.balance
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BalanceRouterConfig {
+
+    @Bean
+    fun abiRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        val routeBuilder = builder.routes()
+
+        routeBuilder.route("balance") {
+            it.path(true, "/balance")
+                .filters { filter ->
+                    filter.rewritePath(
+                        "/balance(?<segment>.*)",
+                        "/\${segment}"
+                    )
+                }
+                .uri("http://defitrack-balance:8080")
+        }
+        return routeBuilder.build()
+    }
+}
