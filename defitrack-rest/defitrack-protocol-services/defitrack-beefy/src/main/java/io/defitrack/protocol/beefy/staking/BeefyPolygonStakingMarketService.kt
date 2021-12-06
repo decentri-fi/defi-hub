7c4709dff0afaa491b305e86f5858dec358ea38a
TheMoneyLisa <moneylisa@defitrack.io>
2021-12-06 17:18:49 +0100

    refactored beefy polygon to use the new cache approach

--- defitrack-rest/defitrack-protocol-services/defitrack-beefy/src/main/java/io/defitrack/protocol/beefy/staking/BeefyPolygonStakingMarketService.kt
+++ defitrack-rest/defitrack-protocol-services/defitrack-beefy/src/main/java/io/defitrack/protocol/beefy/staking/BeefyPolygonStakingMarketService.kt
@@ -1,28 +1,33 @@
 package io.defitrack.protocol.beefy.staking
 
+import io.defitrack.abi.ABIResource
+import io.defitrack.common.network.Network
+import io.defitrack.polygon.config.PolygonContractAccessor
 import io.defitrack.price.PriceRequest
+import io.defitrack.price.PriceResource
+import io.defitrack.protocol.Protocol
+import io.defitrack.protocol.beefy.BeefyService
+import io.defitrack.protocol.beefy.apy.BeefyAPYService
+import io.defitrack.protocol.beefy.contract.BeefyVaultContract
+import io.defitrack.protocol.beefy.domain.BeefyVault
 import io.defitrack.staking.StakingMarketService
 import io.defitrack.staking.domain.RewardToken
 import io.defitrack.staking.domain.StakedToken
 import io.defitrack.staking.domain.StakingMarketElement
 import io.defitrack.token.TokenService
-import io.defitrack.protocol.beefy.apy.BeefyAPYService
-import io.defitrack.abi.ABIResource
-import io.defitrack.price.PriceResource
-import io.defitrack.common.network.Network
-import io.defitrack.polygon.config.PolygonContractAccessor
-import io.defitrack.protocol.Protocol
-import io.defitrack.protocol.beefy.BeefyService
-import io.defitrack.protocol.beefy.contract.BeefyVaultContract
-import io.defitrack.protocol.beefy.domain.BeefyVault
-import okhttp3.internal.toImmutableList
+import io.github.reactivecircus.cache4k.Cache
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 @Service
 class BeefyPolygonStakingMarketService(
@@ -39,30 +44,36 @@ class BeefyPolygonStakingMarketService(
         abiResource.getABI("beefy/VaultV6.json")
     }
 
-    val marketBuffer = mutableListOf<StakingMarketElement>()
 
-    private val executor = Executors.newWorkStealingPool(8)
+    @OptIn(ExperimentalTime::class)
+    val cache =
+        Cache.Builder().expireAfterWrite(Duration.Companion.hours(4)).build<String, List<StakingMarketElement>>()
+
 
     @PostConstruct
-    fun startup() {
-        executor.submit {
-            val vaultContracts = beefyPolygonService.beefyPolygonVaults
-                .map(this::beefyVaultToVaultContract)
+    @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
+    fun init() {
+        Executors.newSingleThreadExecutor().submit {
+            getStakingMarkets()
+        }
+    }
 
-            vaultContracts.forEach { beefyVault ->
-                executor.submit {
-                    importVault(beefyVault)
-                }
+
+    override fun getStakingMarkets(): List<StakingMarketElement> = runBlocking(Dispatchers.IO) {
+        cache.get("all") {
+            val vaultContracts = beefyPolygonService.beefyPolygonVaults
+                .map(::beefyVaultToVaultContract)
+
+            vaultContracts.mapNotNull { beefyVault ->
+                importVault(beefyVault)
             }
         }
     }
 
-    private fun importVault(beefyVault: BeefyVaultContract) {
-        try {
-
+    private fun importVault(beefyVault: BeefyVaultContract): StakingMarketElement? {
+        return try {
             val want = tokenService.getTokenInformation(beefyVault.want, getNetwork())
 
-            logger.info("adding ${beefyVault.name} to beefy vault list")
 
             val element = StakingMarketElement(
                 id = beefyVault.vaultId,
@@ -95,13 +106,14 @@ class BeefyPolygonStakingMarketService(
                 ),
                 vaultType = "beefyVaultV6"
             )
-            marketBuffer.add(element)
+            logger.info("adding ${element.id} to beefy vault list")
+            element
         } catch (ex: Exception) {
             logger.error("Error trying to fetch vault metadata", ex)
+            null
         }
     }
 
-    override fun getStakingMarkets(): List<StakingMarketElement> = marketBuffer.toImmutableList()
 
     private fun getAPY(beefyVault: BeefyVaultContract): Double {
         return try {
