6ac6cd4a846266f2779bee89affaa626ca12fd13
TheMoneyLisa <moneylisa@defitrack.io>
2021-12-06 17:15:03 +0100

    uniswwap pooling market service is now using Cache

--- defitrack-rest/defitrack-protocol-services/defitrack-uniswap/src/main/java/io/defitrack/protocol/uniswap/pooling/UniswapPoolingMarketService.kt
+++ defitrack-rest/defitrack-protocol-services/defitrack-uniswap/src/main/java/io/defitrack/protocol/uniswap/pooling/UniswapPoolingMarketService.kt
@@ -1,16 +1,21 @@
 package io.defitrack.protocol.uniswap.pooling
 
+import io.defitrack.common.network.Network
 import io.defitrack.pool.PoolingMarketService
 import io.defitrack.pool.domain.PoolingMarketElement
 import io.defitrack.pool.domain.PoolingToken
-import io.defitrack.protocol.uniswap.apr.UniswapAPRService
-import io.defitrack.common.network.Network
 import io.defitrack.protocol.Protocol
+import io.defitrack.protocol.uniswap.apr.UniswapAPRService
 import io.defitrack.uniswap.UniswapService
-import okhttp3.internal.toImmutableList
+import io.github.reactivecircus.cache4k.Cache
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 @Component
 class UniswapPoolingMarketService(
@@ -21,12 +26,29 @@ class UniswapPoolingMarketService(
     private val executor = Executors.newCachedThreadPool()
     private val poolingMarkets = mutableListOf<PoolingMarketElement>()
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+    }
+
+
+    @OptIn(ExperimentalTime::class)
+    private val cache = Cache.Builder().expireAfterWrite(
+        Duration.Companion.hours(4)
+    ).build<String, List<PoolingMarketElement>>()
+
     @PostConstruct
-    fun init() {
-        executor.submit {
-            poolingMarkets.clear()
-            uniswapService.getPairs().forEach {
-                val element = PoolingMarketElement(
+    @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
+    fun intitialPopulation() {
+        logger.debug("Fetching pooling markets")
+        Executors.newSingleThreadExecutor().submit {
+            getPoolingMarkets()
+        }
+    }
+
+    fun fetchPoolingMarkets(): List<PoolingMarketElement> {
+        return uniswapService.getPairs().mapNotNull {
+            try {
+                PoolingMarketElement(
                     network = getNetwork(),
                     protocol = getProtocol(),
                     id = "uniswap-ethereum-${it.id}",
@@ -47,14 +69,21 @@ class UniswapPoolingMarketService(
                     apr = uniswapAPRService.getAPR(it.id),
                     marketSize = it.reserveUSD
                 )
-                poolingMarkets.add(element)
+            } catch (ex: Exception) {
+                logger.error("something went wrong trying to import uniswap market ${it.id}")
+                null
             }
         }
     }
 
     override fun getPoolingMarkets(): List<PoolingMarketElement> {
-        return poolingMarkets.toImmutableList()
+        return runBlocking {
+            cache.get("all") {
+                fetchPoolingMarkets()
+            }
+        }
     }
+
 
     override fun getProtocol(): Protocol {
         return Protocol.UNISWAP
