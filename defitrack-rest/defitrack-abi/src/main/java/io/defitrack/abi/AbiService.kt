4dd8d40e632bf966ed205d4b2d512adc6ceafb55
TheMoneyLisa <moneylisa@defitrack.io>
2022-02-03 17:40:44 +0100

    remove the @Cacheable from abi serviced

--- defitrack-rest/defitrack-abi/src/main/java/io/defitrack/abi/AbiService.kt
+++ defitrack-rest/defitrack-abi/src/main/java/io/defitrack/abi/AbiService.kt
@@ -1,5 +1,6 @@
 package io.defitrack.abi
 
+import io.github.reactivecircus.cache4k.Cache
 import io.ktor.client.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
@@ -9,9 +10,10 @@ import org.springframework.stereotype.Component
 @Component
 class AbiService(private val client: HttpClient) {
 
-    @Cacheable(cacheNames = ["abis"], key = "#id")
-    fun getABI(id: String): String {
-        return runBlocking {
+    val cache = Cache.Builder().build<String, String>()
+
+    fun getABI(id: String): String = runBlocking {
+        cache.get(id) {
             client.get("https://raw.githubusercontent.com/defitrack/data/master/abi/${id}")
         }
     }
