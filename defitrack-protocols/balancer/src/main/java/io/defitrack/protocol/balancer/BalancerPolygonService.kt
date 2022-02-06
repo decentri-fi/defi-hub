fb0c9a495c38eebd216f78c6c177b095e3488968
TheMoneyLisa <moneylisa@defitrack.io>
2022-02-06 17:28:11 +0100

    Adding liquidity mining tokens for polygon.balancer.fi

--- defitrack-protocols/balancer/src/main/java/io/defitrack/protocol/balancer/BalancerPolygonService.kt
+++ defitrack-protocols/balancer/src/main/java/io/defitrack/protocol/balancer/BalancerPolygonService.kt
@@ -2,13 +2,7 @@ package io.defitrack.protocol.balancer
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.gson.JsonParser
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.client.features.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import io.ktor.http.*
+import io.defitrack.thegraph.TheGraphGatewayProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Component
@@ -17,8 +11,23 @@ import java.util.*
 @Component
 class BalancerPolygonService(
     private val objectMapper: ObjectMapper,
-    private val client: HttpClient
+    theGraphGatewayProvider: TheGraphGatewayProvider,
 ) {
+
+    private val graphGateway =
+        theGraphGatewayProvider.createTheGraphGateway("https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2-beta")
+
+    companion object {
+        val lmTokens = listOf(
+            "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3", //balancer,
+            "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", //WMATIC,
+            "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756", //TUSD,
+            "0x580a84c73811e1839f75d86d75d88cca0c241ff4", //QI,
+            "0xF501dd45a1198C2E1b5aEF5314A68B9006D842E0", //MTA
+            "0xdf7837de1f2fa4631d716cf2502f8b230f1dcc32" //TEL
+        )
+    }
+
 
     fun getPools(): List<Pool> = runBlocking {
         val query = """
@@ -42,10 +51,8 @@ class BalancerPolygonService(
             }
         """.trimIndent()
 
-        val response = query(query)
-        val poolSharesAsString =
-            JsonParser.parseString(response).asJsonObject["data"].asJsonObject["pools"].toString()
-        return@runBlocking objectMapper.readValue(poolSharesAsString,
+        val pools = graphGateway.performQuery(query).asJsonObject["pools"].toString()
+        return@runBlocking objectMapper.readValue(pools,
             object : TypeReference<List<Pool>>() {
 
             })
@@ -73,20 +80,11 @@ class BalancerPolygonService(
             }
         """.trimIndent()
 
-        val response = query(query)
-        val poolSharesAsString =
-            JsonParser.parseString(response).asJsonObject["data"].asJsonObject["pools"].toString()
-        return@runBlocking objectMapper.readValue(poolSharesAsString,
+        val pools = graphGateway.performQuery(query).asJsonObject["pools"].toString()
+        return@runBlocking objectMapper.readValue(pools,
             object : TypeReference<List<Pool>>() {
 
             }).firstOrNull()
-    }
-
-    fun query(query: String): String = runBlocking {
-        client.request("https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2-beta") {
-            method = HttpMethod.Post
-            body = objectMapper.writeValueAsString(mapOf("query" to query))
-        }
     }
 
     fun getBalances(address: String): List<PoolShare> = runBlocking(Dispatchers.IO) {
@@ -117,11 +115,8 @@ class BalancerPolygonService(
            }
         """.trimIndent()
 
-        val response =
-            query(query)
-        val poolSharesAsString =
-            JsonParser.parseString(response).asJsonObject["data"].asJsonObject["poolShares"].toString()
-        return@runBlocking objectMapper.readValue(poolSharesAsString,
+        val poolShares = graphGateway.performQuery(query).asJsonObject["poolShares"].toString()
+        return@runBlocking objectMapper.readValue(poolShares,
             object : TypeReference<List<PoolShare>>() {
 
             })
