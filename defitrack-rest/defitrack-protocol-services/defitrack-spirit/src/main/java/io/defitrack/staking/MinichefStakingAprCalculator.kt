fc7eaf46c6d43ff3e695f937d124ef35999b5f16
TheMoneyLisa <moneylisa@defitrack.io>
2022-02-10 10:24:19 +0100

    lp tokens standardized

---
+++ defitrack-rest/defitrack-protocol-services/defitrack-spirit/src/main/java/io/defitrack/staking/MinichefStakingAprCalculator.kt
@@ -0,0 +1,55 @@
+package io.defitrack.staking
+
+import io.defitrack.apr.Reward
+import io.defitrack.apr.StakedAsset
+import io.defitrack.apr.StakingAprCalculator
+import io.defitrack.price.PriceResource
+import io.defitrack.protocol.reward.MasterchefLpContract
+import io.defitrack.protocol.staking.Token
+import io.defitrack.protocol.staking.TokenType
+import io.defitrack.token.ERC20Resource
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class MinichefStakingAprCalculator(
+    private val erC20Resource: ERC20Resource,
+    priceResource: PriceResource,
+    private val chef: MasterchefLpContract,
+    private val poolId: Int,
+    private val stakedToken: Token
+) : StakingAprCalculator(priceResource) {
+
+    fun getNativeReward(): Reward {
+        val poolInfo = chef.poolInfo(poolId);
+        val allSushiPerSecond = chef.sushiPerSecond
+        val poolBlockRewards = allSushiPerSecond.times(poolInfo.allocPoint).divide(chef.totalAllocPoint)
+        return Reward(
+            address = chef.rewardToken,
+            network = chef.evmContractAccessor.getNetwork(),
+            amount = poolBlockRewards.toBigDecimal().divide(BigDecimal.TEN.pow(18), 18, RoundingMode.HALF_UP),
+            tokenType = TokenType.SINGLE
+        )
+    }
+
+    override fun getRewardsPerSecond(): List<Reward> {
+        return listOf(
+            getNativeReward()
+        )
+    }
+
+    override fun getStakedTokens(): List<StakedAsset> {
+        val balance = erC20Resource.getBalance(
+            chef.evmContractAccessor.getNetwork(),
+            stakedToken.address,
+            chef.address
+        )
+        return listOf(
+            StakedAsset(
+                address = stakedToken.address,
+                network = chef.evmContractAccessor.getNetwork(),
+                amount = balance.toBigDecimal().divide(BigDecimal.TEN.pow(18), 18, RoundingMode.HALF_UP),
+                tokenType = stakedToken.type
+            )
+        )
+    }
+}
