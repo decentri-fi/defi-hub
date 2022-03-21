76ac81b7bbc65b3bbeba15cbe4adbf4d43740dae
TheMoneyLisa <moneylisa@defitrack.io>
2022-03-21 16:07:25 +0100

    fix logo loop

---
+++ defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/logo/LogoService.kt
@@ -0,0 +1,19 @@
+package io.defitrack.logo
+
+import io.defitrack.common.network.Network
+import org.springframework.stereotype.Service
+import org.web3j.crypto.Keys
+
+@Service
+class LogoService {
+
+    fun generateLogoUrl(network: Network, address: String): String = if (address == "0x0") {
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${network.slug}/info/logo.png"
+    } else {
+           "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${network.slug}/assets/${
+                Keys.toChecksumAddress(
+                    address
+                )
+            }/logo.png"
+    }
+}
