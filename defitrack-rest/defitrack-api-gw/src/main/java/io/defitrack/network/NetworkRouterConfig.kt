650e5d9602d7e0cda72a8bc13f1d9c0aae5dbf16
TheMoneyLisa <moneylisa@defitrack.io>
2022-03-22 16:17:12 +0100

    add network routes

--- defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/network/NetworkRouterConfig.kt
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/network/NetworkRouterConfig.kt
@@ -1,5 +1,8 @@
 package io.defitrack.network
 
+import io.defitrack.common.network.Network
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RequestPredicates.GET
@@ -7,13 +10,31 @@ import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerResponse
 
-
 @Configuration
 class NetworkRouterConfig {
 
     @Bean
-    fun networksRoute(networkHandler: NetworkHandler): RouterFunction<ServerResponse> {
+    fun networkListRoute(networkHandler: NetworkHandler): RouterFunction<ServerResponse> {
         return RouterFunctions
             .route(GET("/networks"), networkHandler::getNetworks)
+    }
+
+    @Bean
+    fun networkRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        val routeBuilder = builder.routes()
+
+        Network.values().forEach { network ->
+            routeBuilder.route(network.name) {
+                it.path(true, "/${network.slug}/**")
+                    .filters { filter ->
+                        filter.rewritePath(
+                            "/${network.slug}/(?<segment>.*)",
+                            "/\${segment}"
+                        )
+                    }
+                    .uri("http://defitrack-${network.slug}:8080")
+            }
+        }
+        return routeBuilder.build()
     }
 }
