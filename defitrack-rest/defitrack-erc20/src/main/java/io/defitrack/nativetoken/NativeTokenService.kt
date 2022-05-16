b7d58a00d79e9323642f767db84ccab811b66d20
TheMoneyLisa <moneylisa@defitrack.io>
2022-05-16 11:35:36 +0200

    add native token

--- defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/nativetoken/NativeTokenService.kt
+++ defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/nativetoken/NativeTokenService.kt
@@ -1,54 +1,60 @@
 package io.defitrack.nativetoken
 
 import io.defitrack.common.network.Network
-import io.defitrack.token.FungibleToken
+import io.defitrack.logo.LogoService
 import io.defitrack.token.TokenInformation
 import io.defitrack.token.TokenType
 import org.springframework.stereotype.Component
 
 @Component
-class NativeTokenService {
+class NativeTokenService(private val logoService: LogoService) {
 
     companion object {
         val nullAddress = "0x0"
     }
 
     fun getNativeToken(network: Network): TokenInformation {
+        val nativeLogo = logoService.generateLogoUrl(network)
         return when (network) {
             Network.ETHEREUM -> TokenInformation(
                 address = nullAddress,
                 name = "Eth",
                 decimals = 18,
                 symbol = "ETH",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo
             )
             Network.ARBITRUM -> TokenInformation(
                 address = nullAddress,
                 name = "Eth",
                 decimals = 18,
                 symbol = "ETH",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo
             )
             Network.POLYGON -> TokenInformation(
                 address = nullAddress,
                 name = "Matic",
                 decimals = 18,
                 symbol = "MATIC",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo
             )
             Network.FANTOM -> TokenInformation(
                 address = nullAddress,
                 name = "Fantom",
                 decimals = 18,
                 symbol = "FTM",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo,
             )
             Network.OPTIMISM -> TokenInformation(
                 address = nullAddress,
                 name = "Eth",
                 decimals = 18,
                 symbol = "ETH",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo,
             )
             Network.AVALANCHE -> TokenInformation(
                 address = nullAddress,
@@ -62,14 +68,16 @@ class NativeTokenService {
                 name = "BNB",
                 decimals = 18,
                 symbol = "BNB",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo,
             )
             Network.POLYGON_MUMBAI -> TokenInformation(
                 address = nullAddress,
                 name = "Matic",
                 decimals = 18,
                 symbol = "MATIC",
-                type = TokenType.NATIVE
+                type = TokenType.NATIVE,
+                logo = nativeLogo,
             )
             else -> throw java.lang.IllegalArgumentException("native token for $network not supported")
         }
