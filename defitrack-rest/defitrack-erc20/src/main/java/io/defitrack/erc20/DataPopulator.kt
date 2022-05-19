f8006c45d4652b803a3b8070ae1d66f8ebe33590
TheMoneyLisa <moneylisa@defitrack.io>
2022-05-19 13:14:34 +0200

    don't block the populator

--- defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/DataPopulator.kt
+++ defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/DataPopulator.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
 
 @Configuration
@@ -16,21 +18,22 @@ class DataPopulator(private val tokenService: TokenService) {
 
     @PostConstruct
     fun start() {
-        runBlocking {
-            val jobs = Network.values()
-                .filter {
-                    it.hasMicroService
-                }.map {
-                    try {
-                        logger.info("Initial population for $it")
-                        val tokens = tokenService.getAllTokensForNetwork(it)
-                        logger.info("Done populating $it with ${tokens.size} elements")
-                    } catch (ex: Exception) {
-                        ex.printStackTrace()
-                        logger.error("${ex.message}")
+        Executors.newSingleThreadExecutor().submit {
+            runBlocking {
+                Network.values()
+                    .filter {
+                        it.hasMicroService
+                    }.map {
+                        try {
+                            logger.info("Initial population for $it")
+                            val tokens = tokenService.getAllTokensForNetwork(it)
+                            logger.info("Done populating $it with ${tokens.size} elements")
+                        } catch (ex: Exception) {
+                            ex.printStackTrace()
+                            logger.error("${ex.message}")
+                        }
                     }
-                }
+            }
         }
-        logger.info("done populating everything")
     }
 }
