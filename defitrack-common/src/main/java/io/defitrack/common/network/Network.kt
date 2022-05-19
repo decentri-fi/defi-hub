ed603dfaa2433bb5023547f1e0a724b6e251c096
TheMoneyLisa <moneylisa@defitrack.io>
2022-05-19 12:03:19 +0200

    fix datapopulator

--- defitrack-common/src/main/java/io/defitrack/common/network/Network.kt
+++ defitrack-common/src/main/java/io/defitrack/common/network/Network.kt
@@ -1,6 +1,6 @@
 package io.defitrack.common.network
 
-enum class Network(val logo: String, val chainId: Int, val slug: String) {
+enum class Network(val logo: String, val chainId: Int, val slug: String, val hasMicroService: Boolean = true) {
     ETHEREUM("ethereum.png", 1, "ethereum"),
     OPTIMISM("optimism.png", 10, "optimism"),
     ARBITRUM("arbitrum.png", 42161, "arbitrum"),
@@ -8,8 +8,7 @@ enum class Network(val logo: String, val chainId: Int, val slug: String) {
     AVALANCHE("avalanche.png", 43114, "avalanche"),
     BINANCE("bsc.svg", 56, "binance"),
     POLYGON("polygon.png", 137, "polygon"),
-    POLYGON_MUMBAI("polygon.png", 80001, "polygon_mumbai");
-
+    POLYGON_MUMBAI("polygon.png", 80001, "polygon_mumbai", hasMicroService = false);
 
     val imageBasePath = "https://static.defitrack.io/images/networks/"
 
--- defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/DataPopulator.kt
+++ defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/DataPopulator.kt
@@ -1,6 +1,7 @@
 package io.defitrack.erc20
 
 import io.defitrack.common.network.Network
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -18,18 +19,22 @@ class DataPopulator(private val tokenService: TokenService) {
     @PostConstruct
     fun start() {
         runBlocking {
-            val jobs = Network.values().map {
-                launch {
-                    try {
-                        logger.info("Initial population for $it")
-                        val tokens = tokenService.getAllTokensForNetwork(it)
-                        logger.info("Done populating $it with ${tokens.size} elements")
-                    } catch (ex: Exception) {
-                        ex.printStackTrace()
-                        logger.error("${ex.message}")
+            val jobs = Network.values()
+                .filter {
+                    it.hasMicroService
+                }
+                .map {
+                    launch(Dispatchers.IO.limitedParallelism(3)) {
+                        try {
+                            logger.info("Initial population for $it")
+                            val tokens = tokenService.getAllTokensForNetwork(it)
+                            logger.info("Done populating $it with ${tokens.size} elements")
+                        } catch (ex: Exception) {
+                            ex.printStackTrace()
+                            logger.error("${ex.message}")
+                        }
                     }
                 }
-            }
 
             jobs.joinAll()
         }
