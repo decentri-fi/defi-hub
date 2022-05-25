535131dd4aee78b138cb6e1aca72d6ddd7cc72f8
TheMoneyLisa <moneylisa@defitrack.io>
2022-05-25 11:52:37 +0200

    add websites to protocols

--- defitrack-common/src/main/java/io/defitrack/protocol/Protocol.kt
+++ defitrack-common/src/main/java/io/defitrack/protocol/Protocol.kt
@@ -4,30 +4,132 @@ enum class Protocol(
     val logo: String,
     val slug: String,
     val primitives: List<DefiPrimitive>,
+    val website: String,
     val enabled: Boolean = true
 ) {
 
-    AAVE("aave.png", "aave", primitives = listOf(DefiPrimitive.LENDING, DefiPrimitive.BORROWING)),
-    HUMANDAO("humandao.jpg", "humandao", primitives = listOf(DefiPrimitive.FARMING)),
-    CURVE("curve.png", "curve", primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    MSTABLE("mstable.png", "mstable", primitives = listOf(DefiPrimitive.LENDING)),
-    COMPOUND("compound.png", "compound", primitives = listOf(DefiPrimitive.LENDING, DefiPrimitive.BORROWING)),
-    BEEFY("beefy.png", "beefy", primitives = listOf(DefiPrimitive.FARMING)),
-    QUICKSWAP("quickswap.png", "quickswap", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    POLYCAT("polycat.webp", "polycat", listOf(DefiPrimitive.FARMING)),
-    HOP("hop.jpg", "hop", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    DINOSWAP("dinoswap.png", "dinoswap", listOf(DefiPrimitive.FARMING)),
-    ADAMANT("adamant.png", "adamant", listOf(DefiPrimitive.FARMING)),
-    UNISWAP("uniswap.png", "uniswap", listOf(DefiPrimitive.POOLING)),
-    DFYN("dfyn.svg", "dfyn", listOf(DefiPrimitive.POOLING)),
-    IDEX("idex.png", "idex", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    SUSHISWAP("sushiswap.png", "sushiswap", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    DMM("dmm.png", "dmm", listOf(DefiPrimitive.POOLING)),
-    BALANCER("balancer.png", "balancer", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    STARGATE("stargate.png", "stargate", listOf(DefiPrimitive.FARMING)),
-    SPOOKY("spooky.png", "spooky", listOf(DefiPrimitive.FARMING)),
-    SPIRITSWAP("spirit.png", "spirit", listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)),
-    CONVEX("convex.png", "convex", listOf(DefiPrimitive.FARMING));
+    AAVE(
+        logo = "aave.png",
+        slug = "aave",
+        website = "https://aave.com/",
+        primitives = listOf(DefiPrimitive.LENDING, DefiPrimitive.BORROWING)
+    ),
+    HUMANDAO(
+        logo = "humandao.jpg",
+        slug = "humandao",
+        website = "https://humandao.org/",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    CURVE(
+        logo = "curve.png",
+        slug = "curve",
+        website = "https://curve.fi",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    MSTABLE(
+        logo = "mstable.png",
+        slug = "mstable",
+        website = "https://mstable.org",
+        primitives = listOf(DefiPrimitive.LENDING)
+    ),
+    COMPOUND(
+        logo = "compound.png",
+        slug = "compound",
+        website = "https://compound.finance",
+        primitives = listOf(DefiPrimitive.LENDING, DefiPrimitive.BORROWING)
+    ),
+    BEEFY(
+        logo = "beefy.png",
+        slug = "beefy",
+        website = "https://beefy.com",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    QUICKSWAP(
+        logo = "quickswap.png",
+        slug = "quickswap",
+        website = "https://quickswap.exchange/",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    POLYCAT(
+        logo = "polycat.webp",
+        slug = "polycat",
+        website = "https://polycat.finance",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    HOP(
+        logo = "hop.jpg",
+        slug = "hop",
+        website = "https://hop.exchange",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    DINOSWAP(
+        logo = "dinoswap.png",
+        slug = "dinoswap",
+        website = "https://dinoswap.exchange",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    ADAMANT(
+        logo = "adamant.png",
+        slug = "adamant",
+        website = "https://adamant.finance",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    UNISWAP(
+        logo = "uniswap.png",
+        slug = "uniswap",
+        website = "https://uniswap.org",
+        primitives = listOf(element = DefiPrimitive.POOLING)
+    ), DFYN(
+        logo = "dfyn.svg",
+        slug = "dfyn",
+        website = "https://dfyn.network",
+        primitives = listOf(DefiPrimitive.POOLING)
+    ),
+    IDEX(
+        logo = "idex.png",
+        slug = "idex",
+        website = "https://idex.io",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    SUSHISWAP(
+        logo = "sushiswap.png",
+        slug = "sushiswap",
+        website = "https://sushi.com",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    DMM(
+        logo = "dmm.png",
+        slug = "dmm",
+        website = "https://kyberswap.com",
+        primitives = listOf(DefiPrimitive.POOLING)
+    ), BALANCER(
+        logo = "balancer.png",
+        slug = "balancer",
+        website = "https://balancer.fi",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ),
+    STARGATE(
+        logo = "stargate.png",
+        slug = "stargate",
+        website = "https://stargate.finance",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ), SPOOKY(
+        logo = "spooky.png",
+        slug = "spooky",
+        website = "https://spooky.fi",
+        primitives = listOf(DefiPrimitive.FARMING)
+    ),
+    SPIRITSWAP(
+        logo = "spirit.png",
+        slug = "spirit",
+        website = "https://www.spiritswap.finance",
+        primitives = listOf(DefiPrimitive.POOLING, DefiPrimitive.FARMING)
+    ), CONVEX(
+        logo = "convex.png",
+        slug = "convex",
+        website = "https://convexfinance.com/",
+        primitives = listOf(DefiPrimitive.FARMING)
+    );
 
     val imageBasePath = "https://static.defitrack.io/images/protocols/"
 
