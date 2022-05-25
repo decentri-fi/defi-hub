aec4baea079250e81fcfa36e824f0f122c8dc7ca
ayushkumar63123 <86553805+ayushkumar63123@users.noreply.github.com>
2022-05-25 17:20:48 +0530

    Create AccountPositions.kt

---
+++ defitrack-protocols/maple/src/main/java/io/defitrack/protocol/maple/contract/AccountPositions.kt
@@ -0,0 +1,10 @@
+package io.defitrack.protocol.maple.contract
+
+//Defining a class of account positions
+class AccountPositions{
+  list: [PoolPosition]
+  totalClaimableLendingReward: String
+  totalClaimableStakingReward: String
+  totalLendingApy: String
+  totalStakingApy: String
+}
