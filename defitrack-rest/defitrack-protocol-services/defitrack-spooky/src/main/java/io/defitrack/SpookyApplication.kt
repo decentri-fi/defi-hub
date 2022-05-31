3ffe546c8f4042e7c6db8d4d2871b8c535a63e60
TheMoneyLisa <moneylisa@defitrack.io>
2022-05-31 13:51:42 +0200

    spooky fix

--- defitrack-rest/defitrack-protocol-services/defitrack-spooky/src/main/java/io/defitrack/SpookyApplication.kt
+++ defitrack-rest/defitrack-protocol-services/defitrack-spooky/src/main/java/io/defitrack/SpookyApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableScheduling
 class SpookyApplication
 
 fun main(args: Array<String>) {
