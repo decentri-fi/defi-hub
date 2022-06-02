23fdd76d701b4325e7727e24980682e140a033a5
korrrba <gitcoiner@gmail.com>
2022-06-02 22:50:31 +0200

    feat: WIP: Add makerdao support to defitrack protocols.
    TODO: this is a modified fork of Aaave V3, needs complete rework for
    MakerDAO.
    
    Ref: https://github.com/defitrack/defitrack-core/issues/16

---
+++ defitrack-protocols/makerdao/src/main/java/io/defitrack/protocol/makerdao/v3/domain/ReserveTokenAddresses.kt
@@ -0,0 +1,7 @@
+package io.defitrack.protocol.makerdao.v3.domain
+
+class ReserveTokenAddresses(
+    val aTokenAddress: String,
+    val stableDebtTokenAddress: String,
+    val variableDebtTokenAddress: String
+)
