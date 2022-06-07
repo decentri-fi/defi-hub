b776aa666823cd6f9f4a0a07888c3b80747d71e6
TheMoneyLisa <moneylisa@defitrack.io>
2022-06-07 11:16:52 +0200

    apeswap pooling markets

---
+++ defitrack-rest/defitrack-protocol-services/defitrack-apeswap/src/main/java/io/defitrack/protocol/apeswap/ApeswapPolygonPoolingMarketProvider.kt
@@ -0,0 +1,43 @@
+package io.defitrack.protocol.apeswap
+
+import io.defitrack.common.network.Network
+import io.defitrack.pool.PoolingMarketService
+import io.defitrack.pool.domain.PoolingMarketElement
+import io.defitrack.protocol.Protocol
+import io.defitrack.token.ERC20Resource
+import io.defitrack.token.TokenType
+import org.springframework.stereotype.Component
+
+@Component
+class ApeswapPolygonPoolingMarketProvider(
+    private val apeswapPolygonGraphProvider: ApeswapPolygonGraphProvider,
+    private val erC20Resource: ERC20Resource
+) :
+    PoolingMarketService() {
+
+    override suspend fun fetchPoolingMarkets(): List<PoolingMarketElement> {
+        return apeswapPolygonGraphProvider.getPools().map { pool ->
+            val liquidityToken = erC20Resource.getTokenInformation(getNetwork(), pool.id)
+            PoolingMarketElement(
+                id = "ape-polygon-${pool.id}",
+                network = getNetwork(),
+                protocol = getProtocol(),
+                address = pool.id,
+                name = liquidityToken.name,
+                symbol = liquidityToken.symbol,
+                tokens = liquidityToken.underlyingTokens.map { it.toFungibleToken() },
+                tokenType = TokenType.APE
+            )
+        }
+    }
+
+    override fun getProtocol(): Protocol {
+        return Protocol.APESWAP
+    }
+
+    override fun getNetwork(): Network {
+        return Network.POLYGON
+    }
+
+
+}
