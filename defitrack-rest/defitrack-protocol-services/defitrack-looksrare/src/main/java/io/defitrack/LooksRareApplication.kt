d50d4fcb96a3b6f9a820ed1e342d022020d765af
TheMoneyLisa <moneylisa@defitrack.io>
2022-06-09 14:40:30 +0200

    looksrare example

---
+++ defitrack-rest/defitrack-protocol-services/defitrack-looksrare/src/main/java/io/defitrack/LooksRareApplication.kt
@@ -0,0 +1,13 @@
+package io.defitrack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+
+@SpringBootApplication
+@EnableCaching
+class LooksRareApplication
+
+fun main(args: Array<String>) {
+    runApplication<LooksRareApplication>(*args)
+}
