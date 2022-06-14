b312715358c20c618fa30fb3b8e324e4630e927a
korrrba <gitcoiner@gmail.com>
2022-06-14 23:55:21 +0200

    defitrack-common: Add networks for convex.

--- defitrack-common/src/main/java/io/defitrack/protocol/Protocol.kt
+++ defitrack-common/src/main/java/io/defitrack/protocol/Protocol.kt
@@ -203,14 +203,15 @@ enum class Protocol(
         logo = "convex.png",
         slug = "convex",
         website = "https://convexfinance.com/",
-        primitives = listOf(DefiPrimitive.FARMING)
+        primitives = listOf(DefiPrimitive.FARMING),
+        networks = listOf(Network.ETHEREUM)
     ),
     MAKERDAO(
         logo = "makerdao.png",
         slug = "makerdao",
         website = "https://makerdao.com/",
         primitives = listOf(DefiPrimitive.LENDING),
-	networks = listOf(Network.ETHEREUM)
+        networks = listOf(Network.ETHEREUM)
     );
 
     val imageBasePath = "https://github.com/defitrack/data/raw/master/logo/protocol/"
