1ff4cceb15857caa01217bece6c058bfc169dbd8
TheMoneyLisa <moneylisa@defitrack.io>
2022-06-29 16:21:20 +0200

    Aave V2 redone

--- defitrack-protocols/aave/src/main/java/io/defitrack/protocol/aave/v2/AaveV2PolygonService.kt
+++ defitrack-protocols/aave/src/main/java/io/defitrack/protocol/aave/v2/AaveV2PolygonService.kt
@@ -56,6 +56,9 @@ class AaveV2PolygonService(
                   liquidityRate
                   variableBorrowRate
                   totalLiquidity
+                  aToken {
+                    id
+                  }
               }
             }
         """.trimIndent()
