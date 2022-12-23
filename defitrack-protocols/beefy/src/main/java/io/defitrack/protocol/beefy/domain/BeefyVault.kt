6a1514a8b4969a7a6b014091cd563c35983c6319
Qkyrie <quintendeswaef@proton.me>
2022-12-23 09:33:32 +0100

    remove unused fields

--- defitrack-protocols/beefy/src/main/java/io/defitrack/protocol/beefy/domain/BeefyVault.kt
+++ defitrack-protocols/beefy/src/main/java/io/defitrack/protocol/beefy/domain/BeefyVault.kt
@@ -4,11 +4,6 @@ class BeefyVault(
     val id: String,
     val name: String,
     val token: String,
-    val tokenAddress: String?,
-    val tokenDecimals: Int,
-    val earnedToken: String,
-    val earnedTokenAddress: String,
     val earnContractAddress: String,
-    val status: String,
     val chain: String,
 )
