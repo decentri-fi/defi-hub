70a89045adc24830a4a70e652f820a01f5004f56
Qkyrie <quintendeswaef@proton.me>
2023-02-21 12:02:32 +0100

    add wbtc arbitrum vault

--- defitrack-protocols/qidao/src/main/java/io/defitrack/protocol/QidaoArbitrumService.kt
+++ defitrack-protocols/qidao/src/main/java/io/defitrack/protocol/QidaoArbitrumService.kt
@@ -7,7 +7,8 @@ class QidaoArbitrumService {
 
     fun provideVaults(): List<String> {
         return listOf(
-            "0xc76a3cbefe490ae4450b2fcc2c38666aa99f7aa0"
+            "0xc76a3cbefe490ae4450b2fcc2c38666aa99f7aa0",
+            "0xb237f4264938f0903f5ec120bb1aa4bee3562fff"
         )
     }
 
