86d4ff3d30a9ec5e90f51da254f2a7a228ea2946
Qkyrie <quintendeswaef@proton.me>
2023-02-21 12:02:36 +0100

    ens router added

---
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/ens/EnsRouterConfig.kt
@@ -0,0 +1,27 @@
+package io.defitrack.ens
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class EnsRouterConfig {
+
+    @Bean
+    fun ensRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        val routeBuilder = builder.routes()
+
+        routeBuilder.route("ens") {
+            it.path(true, "/ens/**")
+                .filters { filter ->
+                    filter.rewritePath(
+                        "/ens/(?<segment>.*)",
+                        "/\${segment}"
+                    )
+                }
+                .uri("http://defitrack-ens:8080")
+        }.build()
+        return routeBuilder.build()
+    }
+}
