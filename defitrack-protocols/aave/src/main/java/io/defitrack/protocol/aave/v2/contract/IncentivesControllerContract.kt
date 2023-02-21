f7ec4e97c1dd32a8f27b54fecd94096586fed060
Qkyrie <quintendeswaef@proton.me>
2023-02-21 12:02:34 +0100

    add incentives for aave v2

---
+++ defitrack-protocols/aave/src/main/java/io/defitrack/protocol/aave/v2/contract/IncentivesControllerContract.kt
@@ -0,0 +1,37 @@
+package io.defitrack.protocol.aave.v2.contract
+
+import io.defitrack.abi.TypeUtils.Companion.toAddress
+import io.defitrack.abi.TypeUtils.Companion.uint256
+import io.defitrack.evm.contract.BlockchainGateway
+import io.defitrack.evm.contract.BlockchainGateway.Companion.MAX_UINT256
+import io.defitrack.evm.contract.EvmContract
+import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.DynamicArray
+import org.web3j.abi.datatypes.Function
+import java.math.BigInteger
+
+class IncentivesControllerContract(
+    blockchainGateway: BlockchainGateway,
+    address: String
+) : EvmContract(
+    blockchainGateway, "", address
+) {
+
+    fun claimRewardsFn(user: String): Function {
+        return createFunction(
+            "claimRewards",
+            inputs = listOf(
+                object : DynamicArray<Address>() {},
+                MAX_UINT256,
+                user.toAddress()
+            )
+        )
+    }
+
+    fun getUserUnclaimedRewardsFn(user: String): Function {
+        return createFunction(
+            "getUserUnclaimedRewards",
+            listOf(user.toAddress()), listOf(uint256())
+        )
+    }
+}
---
+++ defitrack-rest/defitrack-protocol-services/defitrack-aave/src/main/java/io/defitrack/protocol/aave/v2/farming/AaveV2IncentivesControllerMarketProvider.kt
@@ -0,0 +1,49 @@
+package io.defitrack.protocol.aave.v2.farming
+
+import io.defitrack.common.network.Network
+import io.defitrack.market.farming.FarmingMarketProvider
+import io.defitrack.market.farming.domain.FarmingMarket
+import io.defitrack.market.lending.domain.PositionFetcher
+import io.defitrack.protocol.FarmType
+import io.defitrack.protocol.Protocol
+import io.defitrack.protocol.aave.v2.contract.IncentivesControllerContract
+import org.springframework.stereotype.Service
+
+@Service
+class AaveV2IncentivesControllerMarketProvider : FarmingMarketProvider() {
+
+    val stkAave = "0x4da27a545c0c5B758a6BA100e3a049001de870f5"
+    val incentivesController = "0xd784927ff2f95ba542bfc824c8a8a98f3495f6b5"
+
+    override suspend fun fetchMarkets(): List<FarmingMarket> {
+        val stkAaveToken = getToken(stkAave)
+        return listOf(
+            create(
+                name = "Aave v2 incentives controller",
+                identifier = "aave-v2-incentives-controller",
+                stakedToken = stkAaveToken.toFungibleToken(),
+                rewardTokens = listOf(stkAaveToken.toFungibleToken()),
+                vaultType = "aave-v2-incentives",
+                marketSize = getMarketSize(stkAaveToken.toFungibleToken(), incentivesController),
+                balanceFetcher = PositionFetcher(
+                    incentivesController, { user ->
+                        IncentivesControllerContract(
+                            getBlockchainGateway(),
+                            incentivesController
+                        ).getUserUnclaimedRewardsFn(user)
+                    }
+                ),
+                farmType = FarmType.STAKING
+            )
+        )
+    }
+
+    override fun getProtocol(): Protocol {
+        return Protocol.AAVE
+    }
+
+    override fun getNetwork(): Network {
+        return Network.ETHEREUM
+    }
+
+}
