3fb8fd0e647be1dc8f83692bec9d714a89c17a10
Qkyrie <quintendeswaef@proton.me>
2023-02-21 12:02:35 +0100

    transaction fetching

--- defitrack-rest/defitrack-blockchain-services/defitrack-blockchain-service/src/main/java/io/defitrack/contract/TransactionRestController.kt
+++ defitrack-rest/defitrack-blockchain-services/defitrack-blockchain-service/src/main/java/io/defitrack/contract/TransactionRestController.kt
@@ -6,12 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.methods.response.Log
+import org.web3j.protocol.core.methods.response.Transaction
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @RestController
 @RequestMapping("/tx")
 class TransactionRestController(
     private val web3j: Web3j
 ) {
+
+    @OptIn(ExperimentalStdlibApi::class)
+    @GetMapping("/{txId}/logs")
+    fun getTransaction(@PathVariable("txId") txId: String): Transaction? {
+        return web3j.ethGetTransactionByHash(txId).send().transaction.getOrNull()
+    }
+
     @GetMapping("/{txId}/logs")
     fun getLogs(@PathVariable("txId") txId: String): List<Log> {
         return web3j.ethGetTransactionReceipt(txId).send().transactionReceipt.map {
