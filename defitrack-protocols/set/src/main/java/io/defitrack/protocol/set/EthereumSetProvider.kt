f622e9119dafa9e956a57443840fb4ab7e3e4f60
Qkyrie <quintendeswaef@proton.me>
2023-02-21 12:02:33 +0100

    add btc x2 fli

--- defitrack-protocols/set/src/main/java/io/defitrack/protocol/set/EthereumSetProvider.kt
+++ defitrack-protocols/set/src/main/java/io/defitrack/protocol/set/EthereumSetProvider.kt
@@ -7,7 +7,8 @@ class EthereumSetProvider {
 
     fun getSets(): List<String> {
         return listOf(
-            "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd" //eth x2
+            "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd",
+            "0x0b498ff89709d3838a063f1dfa463091f9801c2b"
         )
     }
 }
