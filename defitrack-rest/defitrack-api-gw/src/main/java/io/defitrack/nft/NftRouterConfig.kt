79b9146af95a6918c7919d5e9388601d26da3ad8
Qkyrie <quintendeswaef@proton.me>
2023-03-06 17:29:39 +0100

    package conv

--- defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/nft/NftRouterConfig.kt
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/nft/NftRouterConfig.kt
@@ -1,4 +1,4 @@
-package io.defitrack.ens
+package io.defitrack.nft
 
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
