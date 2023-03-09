19d84a27bc67fc8d5a96a82390920736a72dbb18
Qkyrie <quintendeswaef@proton.me>
2023-03-09 11:19:12 +0100

    minor cleaning

--- defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/protocolspecific/SpookyTokenIdentifier.kt
+++ defitrack-rest/defitrack-erc20/src/main/java/io/defitrack/erc20/protocolspecific/SpookyTokenIdentifier.kt
@@ -3,12 +3,11 @@ package io.defitrack.erc20.protocolspecific
 import io.defitrack.erc20.ERC20
 import io.defitrack.erc20.LpContractReader
 import io.defitrack.protocol.Protocol
-import io.defitrack.token.ERC20Resource
 import io.defitrack.token.TokenType
 import org.springframework.stereotype.Component
 
 @Component
-class SpookkyTokenIdentifier(
+class SpookyTokenIdentifier(
     lpContractReader: LpContractReader
 ) : DefaultLpIdentifier(
     Protocol.SPOOKY, TokenType.SPOOKY, lpContractReader,
