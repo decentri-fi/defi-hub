d3030f4601e282aa046a813880ecf22c5b576cd3
Qkyrie <quintendeswaef@proton.me>
2023-03-27 10:55:19 +0200

    hateaos

--- defitrack-rest/defitrack-protocol-services/defitrack-protocol-service/src/main/java/io/defitrack/protocol/ProtocolRestController.kt
+++ defitrack-rest/defitrack-protocol-services/defitrack-protocol-service/src/main/java/io/defitrack/protocol/ProtocolRestController.kt
@@ -1,6 +1,8 @@
 package io.defitrack.protocol
 
 import io.defitrack.market.farming.DefaultFarmingMarketRestController
+import io.defitrack.market.lending.DefaultLendingMarketsRestController
+import io.defitrack.market.pooling.DefaultPoolingMarketRestController
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,8 +18,15 @@ class ProtocolRestController(
     fun getProtocol(): ProtocolVO {
         val protocol = protocolProvider.getProtocol().toVO()
         return with(protocol) {
-            val farming = linkTo(DefaultFarmingMarketRestController::class.java).slash("all-markets").withRel("farming")
-            add(farming)
+            if(protocol.primitives.contains(DefiPrimitive.FARMING)) {
+                add(linkTo(DefaultFarmingMarketRestController::class.java).withRel("farming"))
+            }
+            if(protocol.primitives.contains(DefiPrimitive.POOLING)) {
+                add(linkTo(DefaultPoolingMarketRestController::class.java).withRel("pooling"))
+            }
+            if(protocol.primitives.contains(DefiPrimitive.LENDING)) {
+                add(linkTo(DefaultLendingMarketsRestController::class.java).withRel("lending"))
+            }
             this
         }
     }
