7020210b8a68b60673c0473a3f20705eb67920cc
Qkyrie <quintendeswaef@proton.me>
2023-03-31 13:17:45 +0200

    parallel calls for token balances

--- defitrack-rest/defitrack-balance/src/main/java/io/defitrack/balance/rest/BalanceRestController.kt
+++ defitrack-rest/defitrack-balance/src/main/java/io/defitrack/balance/rest/BalanceRestController.kt
@@ -2,6 +2,7 @@ package io.defitrack.balance.rest
 
 import io.defitrack.balance.service.BalanceService
 import io.defitrack.balance.service.dto.BalanceElement
+import io.defitrack.balance.service.dto.TokenBalance
 import io.defitrack.common.network.Network
 import io.defitrack.common.utils.FormatUtilsExtensions.asEth
 import io.defitrack.network.toVO
@@ -12,7 +13,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import java.math.BigDecimal
 
@@ -24,8 +24,6 @@ class BalanceRestController(
     private val erC20Resource: ERC20Resource,
 ) {
 
-    val logger = LoggerFactory.getLogger(this::class.java)
-
     @Deprecated("use the network-specific call")
     @GetMapping("/{address}/native-balance")
     fun getBalance(@PathVariable("address") address: String): List<BalanceElement> = runBlocking {
@@ -36,17 +34,14 @@ class BalanceRestController(
                 if (balance > BigDecimal.ZERO) {
                     val price = priceResource.calculatePrice(
                         PriceRequest(
-                            "0x0",
-                            it.getNetwork(),
-                            1.0.toBigDecimal()
+                            "0x0", it.getNetwork(), 1.0.toBigDecimal()
                         )
                     )
                     BalanceElement(
                         amount = balance.toDouble(),
                         network = it.getNetwork().toVO(),
                         token = erC20Resource.getTokenInformation(
-                            it.getNetwork(),
-                            "0x0"
+                            it.getNetwork(), "0x0"
                         ).toFungibleToken(),
                         dollarValue = price.times(balance.toDouble()),
                         price = price
@@ -60,8 +55,7 @@ class BalanceRestController(
 
     @GetMapping(value = ["/{address}/native-balance"], params = ["network"])
     fun getBalanceByNetwork(
-        @PathVariable("address") address: String,
-        @RequestParam("network") network: Network
+        @PathVariable("address") address: String, @RequestParam("network") network: Network
     ): BalanceElement = runBlocking {
         val balanceService = balanceServices.first {
             it.getNetwork() == network
@@ -77,74 +71,50 @@ class BalanceRestController(
         )
 
         BalanceElement(
-            amount = balance.toDouble(),
-            network = network.toVO(),
-            token = erC20Resource.getTokenInformation(
-                network,
-                "0x0"
-            ).toFungibleToken(),
-            dollarValue = price.times(balance.toDouble()),
-            price = price
+            amount = balance.toDouble(), network = network.toVO(), token = erC20Resource.getTokenInformation(
+                network, "0x0"
+            ).toFungibleToken(), dollarValue = price.times(balance.toDouble()), price = price
         )
     }
 
     @GetMapping("/{address}/token-balances")
     fun getTokenBalance(@PathVariable("address") address: String): List<BalanceElement> = runBlocking {
-        balanceServices.flatMap {
-            try {
-                it.getTokenBalances(address)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-                emptyList()
+        balanceServices.map {
+            async {
+                try {
+                    it.getTokenBalances(address)
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    emptyList()
+                }
             }
-        }.map {
-            val normalizedAmount = it.amount.asEth(it.token.decimals).toDouble()
-            val price = priceResource.calculatePrice(
-                PriceRequest(
-                    it.token.address,
-                    it.network,
-                    1.0.toBigDecimal()
-                )
-            )
-
-            BalanceElement(
-                amount = normalizedAmount,
-                network = it.network.toVO(),
-                token = it.token,
-                dollarValue = price.times(normalizedAmount),
-                price = price
-            )
-        }
+        }.awaitAll().flatten().map { it.toBalanceElement() }
     }
 
     @GetMapping(value = ["/{address}/token-balances"], params = ["network"])
     fun getTokenBalanceByNetwork(
-        @PathVariable("address") address: String,
-        @RequestParam("network") network: Network
+        @PathVariable("address") address: String, @RequestParam("network") network: Network
     ): List<BalanceElement> = runBlocking(Dispatchers.IO) {
         val balanceService = balanceServices.firstOrNull {
             it.getNetwork() == network
         }
 
-        balanceService?.getTokenBalances(address)?.map {
-            val normalizedAmount = it.amount.asEth(it.token.decimals).toDouble()
-            val price = priceResource.calculatePrice(
-                PriceRequest(
-                    it.token.address,
-                    it.network,
-                    1.0.toBigDecimal()
-                )
+        balanceService?.getTokenBalances(address)?.map { it.toBalanceElement() } ?: emptyList()
+    }
+
+    suspend fun TokenBalance.toBalanceElement(): BalanceElement {
+        val normalizedAmount = amount.asEth(token.decimals).toDouble()
+        val price = priceResource.calculatePrice(
+            PriceRequest(
+                token.address, network, 1.0.toBigDecimal()
             )
-            BalanceElement(
-                amount = normalizedAmount,
-                network = it.network.toVO(),
-                token = it.token,
-                dollarValue = price.times(normalizedAmount),
-                price = price
-            )
-        } ?: kotlin.run {
-            logger.error("no balance service found for network $network")
-            emptyList()
-        }
+        )
+        return BalanceElement(
+            amount = normalizedAmount,
+            network = network.toVO(),
+            token = token,
+            dollarValue = price.times(normalizedAmount),
+            price = price
+        )
     }
 }
