00d1c99743587893ceaf7c2603a71f4f6973cbe9
Qkyrie <quintendeswaef@proton.me>
2023-05-10 11:30:35 +0200

    Fix the build config

--- ci/package.sh
+++ ci/package.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/bash
 
 function startNetwork {
-  docker build -t ${DOCKER_REPO}:$1-${BRANCH_NAME} defitrack-rest/defitrack-blockchain-services/defitrack-$1 -f ci/Dockerfile
+  # docker build -t ${DOCKER_REPO}:$1-${BRANCH_NAME} defitrack-rest/defitrack-blockchain-services/defitrack-$1 -f ci/Dockerfile
+  ./mvnw -pl defitrack-rest/defitrack-blockchain-services/defitrack-$1 spring-boot:build-image
+
 }
 
 function startProtocol {
-  mvn -pl defitrack-protocol-services/defitrack-$1 spring-boot:build-image
+  ./mvnw -pl defitrack-rest/defitrack-protocol-services/defitrack-$1 spring-boot:build-image
   #docker build -t ${DOCKER_REPO}:$1-${BRANCH_NAME} defitrack-rest/defitrack-protocol-services/defitrack-$1 -f ci/Dockerfile
 }
 
 
 function startInfra {
   # docker build -t ${DOCKER_REPO}:$1-${BRANCH_NAME} defitrack-rest/defitrack-$1 -f ci/Dockerfile
-  mvn -pl defitrack-rest/defitrack-$1 spring-boot:build-image
+  ./mvnw -pl defitrack-rest/defitrack-$1 spring-boot:build-image
 }
 
 git clone https://github.com/decentri-fi/infrastructure
