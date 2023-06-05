c7a254755fb67183e93337c26cf6b742014babfa
Qkyrie <quintendeswaef@proton.me>
2023-06-05 15:41:36 +0200

    fix idesx

--- defitrack-rest/defitrack-protocol-services/defitrack-idex/src/main/java/io/defitrack/IdexApplication.kt
+++ defitrack-rest/defitrack-protocol-services/defitrack-idex/src/main/java/io/defitrack/IdexApplication.kt
@@ -1,14 +1,16 @@
 package io.defitrack
 
+import io.defitrack.protocol.Company
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
-@EnableCaching
-@EnableScheduling
-class IdexApplication
+class IdexApplication :   ProtocolApplication() {
+    override fun getCompany(): Company {
+        return Company.IDEX
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<IdexApplication>(*args)
