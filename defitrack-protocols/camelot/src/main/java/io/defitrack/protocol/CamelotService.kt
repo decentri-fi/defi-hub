0287fc9289ae30e1f9668fca6927334e495df247
Qkyrie <quintendeswaef@proton.me>
2023-06-06 14:18:20 +0200

    optimize camelot

--- defitrack-protocols/camelot/src/main/java/io/defitrack/protocol/CamelotService.kt
+++ defitrack-protocols/camelot/src/main/java/io/defitrack/protocol/CamelotService.kt
@@ -7,7 +7,6 @@ import io.defitrack.protocol.algebra.AlgebraPoolContract
 import io.defitrack.protocol.algebra.AlgebraPosition
 import io.defitrack.protocol.algebra.AlgebraPositionsV2Contract
 import io.github.reactivecircus.cache4k.Cache
-import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
@@ -15,8 +14,8 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.Executors
 import kotlin.time.Duration.Companion.hours
 
 private const val CAMELOT_NFT = "0xacdcc3c6a2339d08e0ac9f694e4de7c52f890db3"
@@ -45,34 +44,35 @@ class CamelotService(
 
     val cache = Cache.Builder<String, List<AlgebraPoolContract>>().expireAfterAccess(1.hours).build()
 
-    @PostConstruct
+    @Scheduled(fixedDelay = 1000 * 60 * 60 * 24) // 24 hours
     fun init() = runBlocking {
-        Executors.newSingleThreadExecutor().submit {
-            runBlocking {
-                logger.info("importing camelot markets")
-                cache.put("all", getPools())
-                logger.info("done importing camelot markets")
-            }
-        }
+        logger.info("importing camelot markets")
+        val pools = fetchPools()
+        cache.put("all", pools)
+        logger.info("done importing ${pools.size} camelot markets")
     }
 
     suspend fun getPools(): List<AlgebraPoolContract> = coroutineScope {
         cache.get("all") {
-            val sema = Semaphore(16)
-            getAllPositions().distinctBy {
-                listOf(it.token0, it.token1).sorted().joinToString("-")
-            }.map {
-                async {
-                    sema.withPermit {
-                        algebraFactoryContract.getPoolByPair(it.token0, it.token1)
-                    }
+            fetchPools()
+        }
+    }
+
+    private suspend fun fetchPools(): List<AlgebraPoolContract> = coroutineScope {
+        val sema = Semaphore(16)
+        getAllPositions().distinctBy {
+            listOf(it.token0, it.token1).sorted().joinToString("-")
+        }.map {
+            async {
+                sema.withPermit {
+                    algebraFactoryContract.getPoolByPair(it.token0, it.token1)
                 }
-            }.awaitAll().distinct().map {
-                AlgebraPoolContract(
-                    blockchainGatewayProvider.getGateway(Network.ARBITRUM),
-                    it
-                )
             }
+        }.awaitAll().distinct().map {
+            AlgebraPoolContract(
+                blockchainGatewayProvider.getGateway(Network.ARBITRUM),
+                it
+            )
         }
     }
 
