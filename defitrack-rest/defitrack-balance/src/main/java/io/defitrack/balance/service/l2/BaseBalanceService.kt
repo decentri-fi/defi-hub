f80db9daabf834801c9a22b0e261cc915444788a
Quinten De Swaef <quintendeswaef@gmail.com>
2023-08-25 14:40:03 +0200

    add base

---
+++ defitrack-rest/defitrack-balance/src/main/java/io/defitrack/balance/service/l2/BaseBalanceService.kt
@@ -0,0 +1,21 @@
+package io.defitrack.balance.service.l2
+
+import io.defitrack.balance.service.BalanceService
+import io.defitrack.common.network.Network
+import io.defitrack.evm.contract.BlockchainGatewayProvider
+import io.defitrack.token.ERC20Resource
+import org.springframework.stereotype.Service
+
+@Service
+class BaseBalanceService(
+    blockchainGatewayProvider: BlockchainGatewayProvider,
+    erc20Resource: ERC20Resource,
+) : BalanceService(blockchainGatewayProvider, erc20Resource) {
+
+
+    override fun getNetwork(): Network = Network.BASE
+
+    override fun nativeTokenName(): String {
+        return "ETH"
+    }
+}
