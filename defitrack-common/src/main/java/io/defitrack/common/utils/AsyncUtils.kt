598fc33fb1711e0dc62d592f8da6d881c6a71384
Quinten De Swaef <quintendeswaef@gmail.com>
2023-09-07 12:11:28 +0200

    utils for doing lazy evaluation of coroutinescoped variables

--- defitrack-common/src/main/java/io/defitrack/common/utils/AsyncUtils.kt
+++ defitrack-common/src/main/java/io/defitrack/common/utils/AsyncUtils.kt
@@ -1,10 +1,15 @@
 package io.defitrack.common.utils
 
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.*
 
 object AsyncUtils {
 
     suspend fun <T> Deferred<T>.await(timeout: Long, defaultValue: T) =
         withTimeoutOrNull(timeout) { await() } ?: defaultValue
+
+    fun <T> lazyAsync(block: suspend kotlinx.coroutines.CoroutineScope.() -> T): Deferred<T> {
+        return GlobalScope.async(Dispatchers.Unconfined, start = CoroutineStart.LAZY) {
+            block()
+        }
+    }
 }
