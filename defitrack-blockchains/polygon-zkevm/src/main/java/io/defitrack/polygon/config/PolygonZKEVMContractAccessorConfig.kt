d36d1eda14a696feda62af6ee501f88dd5e54cae
Quinten De Swaef <quintendeswaef@gmail.com>
2023-09-12 14:16:53 +0200

    Polygon ZKEvm - support multicall v2

--- defitrack-blockchains/polygon-zkevm/src/main/java/io/defitrack/polygon/config/PolygonZKEVMContractAccessorConfig.kt
+++ defitrack-blockchains/polygon-zkevm/src/main/java/io/defitrack/polygon/config/PolygonZKEVMContractAccessorConfig.kt
@@ -4,6 +4,7 @@ import io.defitrack.common.network.Network
 import io.defitrack.evm.abi.AbiDecoder
 import io.defitrack.evm.contract.BlockchainGateway
 import io.defitrack.evm.contract.multicall.MultiCallV1Caller
+import io.defitrack.evm.contract.multicall.MultiCallV2Caller
 import io.ktor.client.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -21,7 +22,7 @@ class PolygonZKEVMContractAccessorConfig(
         return BlockchainGateway(
             abiDecoder,
             Network.POLYGON_ZKEVM,
-            MultiCallV1Caller("0xfB4C2947223ED76452Ce43D5afda2bcc90D42545"),
+            MultiCallV2Caller("0x7f593DEdebE173CA12954164dc3db56131FCA0F7"),
             httpClient,
             endpoint
         )
