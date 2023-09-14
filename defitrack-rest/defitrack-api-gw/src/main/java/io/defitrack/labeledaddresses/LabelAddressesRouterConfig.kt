9a4ca82db9111015a1fad255fa4ce0fa47bbf7f1
Quinten De Swaef <quintendeswaef@gmail.com>
2023-09-14 08:01:42 +0200

    General - Fix gateway

--- defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/labeledaddresses/LabelAddressesRouterConfig.kt
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/labeledaddresses/LabelAddressesRouterConfig.kt
@@ -20,7 +20,7 @@ class LabelAddressesRouterConfig {
                         "/\${segment}"
                     )
                 }
-                .uri("http://labeled-addresses:8080.default.svc.cluster.local")
+                .uri("http://labeled-addresses.default.svc.cluster.local:8080")
         }.route("labeladdresses") {
             it.path(true, "/labeled-addresses/**")
                 .filters { filter ->
