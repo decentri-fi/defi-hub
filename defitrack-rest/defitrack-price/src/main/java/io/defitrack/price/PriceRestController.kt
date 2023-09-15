6fd28aed666fc441a084bb37164f27fd2968dd88
Quinten De Swaef <quintendeswaef@gmail.com>
2023-09-15 15:42:13 +0200

    Price API - add single endpoint for price fetching of a token

--- defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/PriceRestController.kt
+++ defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/PriceRestController.kt
@@ -1,9 +1,15 @@
 package io.defitrack.price
 
+import io.defitrack.common.network.Network
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 
 @RestController
 @RequestMapping("/")
@@ -13,5 +19,29 @@ class PriceRestController(
     @PostMapping
     suspend fun calculatePrice(@RequestBody priceRequest: PriceRequest): Double {
         return priceCalculator.calculatePrice(priceRequest)
+    }
+
+
+    @GetMapping("/{address}")
+    suspend fun getPricePerToken(
+        @PathVariable("address") address: String,
+        @RequestParam("network") networkName: String
+    ): Map<String, Any> {
+
+        val network = Network.fromString(networkName) ?: ResponseEntity.ok(
+            mapOf(
+                "price" to BigDecimal.ZERO
+            )
+        )
+
+        return mapOf(
+            "price" to priceCalculator.calculatePrice(
+                PriceRequest(
+                    address,
+                    network,
+                    BigDecimal.ONE
+                )
+            )
+        )
     }
 }
