51ecf86336c5fdeebba26b48b41a7c5d11bf4faa
Quinten De Swaef <quintendeswaef@gmail.com>
2023-09-25 10:16:37 +0200

    Claimables - start of aggregate service

---
+++ defitrack-rest/defitrack-claimables/src/main/java/io/defitrack/DefitrackClaimablesApp.kt
@@ -0,0 +1,13 @@
+package io.defitrack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+class DefitrackAbiApp
+
+fun main(args: Array<String>) {
+    runApplication<DefitrackAbiApp>(*args)
+}
