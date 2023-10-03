ed9a237f040ed3f5d52eb876789967c8bca43cd5
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-03 13:42:53 +0200

    General: Big ass refactor

--- defitrack-rest/defitrack-protocol-service/src/main/java/defitrack/UnifiedProtocolApp.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/defitrack/UnifiedProtocolApp.kt
@@ -2,9 +2,13 @@ package io.defitrack
 
 import io.defitrack.protocol.Company
 import io.defitrack.protocol.CompanyProvider
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.availability.AvailabilityChangeEvent
 import org.springframework.boot.availability.ReadinessState
+import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
@@ -15,22 +19,38 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableCaching
 @EnableScheduling
 @EnableAsync
-abstract class ProtocolApplication(applicationContext: ApplicationContext) {
+class UnifiedProtocolApp(
+    @Value("\${decentrifi.company}") private val company: String,
+    applicationContext: ApplicationContext
+){
 
     init {
         AvailabilityChangeEvent.publish(applicationContext, ReadinessState.REFUSING_TRAFFIC)
     }
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun init() {
+        logger.info("Starting ${getCompany().name} protocol")
+    }
 
     @Bean
     fun provideCompany(): CompanyProvider {
         return object : CompanyProvider {
             override fun getCompany(): Company {
-                return this@ProtocolApplication.getCompany()
+                return this@UnifiedProtocolApp.getCompany()
             }
         }
     }
 
-    abstract fun getCompany(): Company
+    fun getCompany(): Company {
+        return Company.entries.firstOrNull {
+            it.slug.lowercase() == company.lowercase() || it.name.lowercase() == company.lowercase()
+        } ?: throw IllegalArgumentException("Company $company not found")
+    }
+}
 
+fun main(args: Array<String>) {
+    runApplication<UnifiedProtocolApp>(*args)
 }
