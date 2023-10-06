2dff8437fc32b2eed8e4bc839649156e145b9d8c
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-06 15:28:26 +0200

    ENS: swagger docs

--- defitrack-rest/defitrack-ens/src/main/java/io/defitrack/ens/rest/ENSRestController.kt
+++ defitrack-rest/defitrack-ens/src/main/java/io/defitrack/ens/rest/ENSRestController.kt
@@ -5,18 +5,25 @@ import io.defitrack.ens.domain.ENSResult
 import io.defitrack.ens.service.EnsNameService
 import io.micrometer.observation.Observation
 import io.micrometer.observation.ObservationRegistry
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Tag(
+    name = "ENS",
+    description = "ENS API, enabling us to easily fetch ens information."
+)
 class ENSRestController(
     private val ensNameService: EnsNameService,
     private val observationRegistry: ObservationRegistry
 ) {
 
     @GetMapping("/by-name/{ens}")
+    @Operation(summary = "Get ENS information by ens-name")
     fun getAddressInformation(@PathVariable("ens") ens: String): ENSResult = runBlocking {
         val observation = Observation.start("ens_by_name", observationRegistry)
 
@@ -32,6 +39,7 @@ class ENSRestController(
     }
 
     @GetMapping("/by-address/{address}")
+    @Operation(summary = "Get ENS information by address")
     fun getMapping(@PathVariable("address") address: String): ENSResult = runBlocking {
         val observation = Observation.start("ens_by_address", observationRegistry)
 
@@ -49,7 +57,7 @@ class ENSRestController(
     }
 
     @GetMapping("/by-name/{name}/avatar")
-    @Trace(metricName = "controller.ens.by-name.avatar")
+    @Operation(summary = "Get avatar information by ens-name")
     suspend fun getAvatar(@PathVariable name: String): Map<String, String> {
         val result = ensNameService.getAvatar(name)
         return if (result.isNotBlank()) {
