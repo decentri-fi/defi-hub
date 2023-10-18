9b629a92c461a1d487b46adb428d596bc88e07cd
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-18 13:14:11 +0200

    Fix build

--- defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/DefitrackApiGwApplication.kt
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/DefitrackApiGwApplication.kt
@@ -2,6 +2,7 @@ package io.defitrack
 
 import io.defitrack.protocol.distribution.ProtocolDistributionConfig
 import jakarta.annotation.PostConstruct
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -13,7 +14,7 @@ class DefitrackApiGwApplication(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @PostConstruct
-    fun init() {
+    fun init() = runBlocking {
         logger.info("startup with distsributionconfig")
         protocolDistributionConfig.getConfigs().forEach {
             logger.info("${it.name}: ${it.companies.joinToString(",") { it.slug }}")
