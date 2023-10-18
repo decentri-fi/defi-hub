300122727c7537fc993fc6ac44fc0080ec3ea273
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-18 09:53:44 +0200

    better logging and exception handling for pooling position providers

--- defitrack-rest/defitrack-api/src/main/java/io/defitrack/market/pooling/DefaultPoolingPositionProvider.kt
+++ defitrack-rest/defitrack-api/src/main/java/io/defitrack/market/pooling/DefaultPoolingPositionProvider.kt
@@ -1,10 +1,15 @@
 package io.defitrack.market.pooling
 
+import arrow.core.Either.Companion.catch
+import arrow.core.None
+import arrow.core.some
 import io.defitrack.evm.contract.BlockchainGatewayProvider
 import io.defitrack.market.pooling.domain.PoolingPosition
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.math.BigInteger
@@ -17,43 +22,46 @@ class DefaultPoolingPositionProvider(
 
     val logger = LoggerFactory.getLogger(this::class.java)
 
+    val semaphore = Semaphore(16)
+
     override suspend fun fetchUserPoolings(protocol: String, address: String): List<PoolingPosition> = coroutineScope {
         poolingMarketProviders
-            .filter {
-                it.getProtocol().slug == protocol
-            }
-            .map { provider ->
-                return@map async {
-                    try {
-                        val markets = provider.getMarkets().filter { it.positionFetcher != null }
-
-                        if (markets.isEmpty()) {
-                            return@async emptyList()
-                        }
-
-                        gateway.getGateway(provider.getNetwork()).readMultiCall(
-                            markets.map { market ->
-                                market.positionFetcher!!.toMulticall(address)
-                            }
-                        ).mapIndexed { index, retVal ->
-                            val market = markets[index]
-                            val position = market.positionFetcher!!.extractBalance(retVal.data)
-
-                            if (position.underlyingAmount > BigInteger.ONE) {
-                                PoolingPosition(
-                                    position.tokenAmount,
-                                    market
-                                )
-                            } else {
-                                null
-                            }
-                        }.filterNotNull()
-                    } catch (ex: Exception) {
-                        logger.error("Unable to fetch user poolings for provider ${provider.javaClass}: ${ex.message}")
-                        emptyList()
-                    }
+            .filter { it.getProtocol().slug == protocol }
+            .flatMap { provider ->
+                val markets = provider.getMarkets().filter { it.positionFetcher != null }
+                if (markets.isEmpty()) {
+                    return@flatMap emptyList()
                 }
-            }.awaitAll().flatten()
-    }
 
+                gateway.getGateway(provider.getNetwork()).readMultiCall(
+                    markets.map { market ->
+                        market.positionFetcher!!.toMulticall(address)
+                    }
+                ).mapIndexed { index, retVal ->
+                   async {
+                       semaphore.withPermit {
+                           val market = markets[index]
+                           catch {
+                               val position = market.positionFetcher!!.extractBalance(retVal.data)
+
+                               if (position.underlyingAmount > BigInteger.ONE) {
+                                   PoolingPosition(
+                                       position.tokenAmount,
+                                       market
+                                   ).some()
+                               } else {
+                                   None
+                               }
+                           }.mapLeft {
+                               logger.error("Error fetching balance for ${market.name}", it)
+                           }.map {
+                               it.getOrNull()
+                           }
+                       }
+                   }
+                }
+            }
+    }.awaitAll().mapNotNull {
+        it.getOrNull()
+    }
 }
