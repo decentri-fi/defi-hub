4f7144f1b035627746ede3b1b6b4630480f8de1b
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-19 14:08:06 +0200

    simplify adamant

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/adamant/staking/AdamantVaultMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/adamant/staking/AdamantVaultMarketProvider.kt
@@ -1,9 +1,10 @@
 package io.defitrack.protocol.adamant.staking
 
+import arrow.core.Either.Companion.catch
+import arrow.fx.coroutines.parMap
 import io.defitrack.claimable.domain.ClaimableRewardFetcher
 import io.defitrack.claimable.domain.Reward
 import io.defitrack.common.network.Network
-import io.defitrack.common.utils.AsyncUtils.lazyAsync
 import io.defitrack.conditional.ConditionalOnCompany
 import io.defitrack.evm.contract.ERC20Contract.Companion.balanceOfFunction
 import io.defitrack.market.farming.FarmingMarketProvider
@@ -14,10 +15,8 @@ import io.defitrack.protocol.Protocol
 import io.defitrack.protocol.adamant.AdamantService
 import io.defitrack.protocol.adamant.AdamantVaultContract
 import io.defitrack.transaction.PreparedTransaction.Companion.selfExecutingTransaction
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Component
+import kotlin.coroutines.EmptyCoroutineContext
 
 @Component
 @ConditionalOnCompany(Company.ADAMANT)
@@ -29,48 +28,47 @@ class AdamantVaultMarketProvider(
         return Protocol.ADAMANT
     }
 
-    val addy = lazyAsync {
-        getToken("0xc3fdbadc7c795ef1d6ba111e06ff8f16a20ea539")
+    override suspend fun fetchMarkets(): List<FarmingMarket> {
+        val addy = getToken("0xc3fdbadc7c795ef1d6ba111e06ff8f16a20ea539")
+
+        return getVaults().parMap(EmptyCoroutineContext, 12) { vault ->
+              catch {
+                  val token = getToken(vault.token())
+                  val rewardToken = addy.toFungibleToken()
+                  create(
+                      name = "${token.name} vault",
+                      identifier = vault.address,
+                      stakedToken = token.toFungibleToken(),
+                      rewardTokens = listOf(
+                          rewardToken
+                      ),
+                      positionFetcher = PositionFetcher(
+                          vault.address,
+                          { user -> balanceOfFunction(user) }
+                      ),
+                      claimableRewardFetcher = ClaimableRewardFetcher(
+                          Reward(
+                              token = rewardToken,
+                              contractAddress = vault.address,
+                              getRewardFunction = vault::getPendingRewardFunction
+                          ),
+                          preparedTransaction = selfExecutingTransaction(vault::getClaimFunction)
+                      )
+                  )
+              }
+        }.mapNotNull {
+            it.mapLeft {
+                logger.error("Error fetching vault", it)
+            }.getOrNull()
+        }
     }
 
-    override suspend fun fetchMarkets(): List<FarmingMarket> =
-        coroutineScope {
-            adamantService.adamantGenericVaults().map {
-                AdamantVaultContract(
-                    getBlockchainGateway(),
-                    it.vaultAddress
-                )
-            }.map { vault ->
-                async {
-                    try {
-                        val token = getToken(vault.token())
-                        val rewardToken = addy.await().toFungibleToken()
-                        create(
-                            name = "${token.name} vault",
-                            identifier = vault.address,
-                            stakedToken = token.toFungibleToken(),
-                            rewardTokens = listOf(
-                                rewardToken
-                            ),
-                            positionFetcher = PositionFetcher(
-                                vault.address,
-                                { user -> balanceOfFunction(user) }
-                            ),
-                            claimableRewardFetcher = ClaimableRewardFetcher(
-                                Reward(
-                                    token = rewardToken,
-                                    contractAddress = vault.address,
-                                    getRewardFunction = vault::getPendingRewardFunction
-                                ),
-                                preparedTransaction = selfExecutingTransaction(vault::getClaimFunction)
-                            )
-                        )
-                    } catch (ex: Exception) {
-                        null
-                    }
-                }
-            }.awaitAll().filterNotNull()
-        }
+    private suspend fun getVaults() = adamantService.adamantGenericVaults().map {
+        AdamantVaultContract(
+            getBlockchainGateway(),
+            it.vaultAddress
+        )
+    }
 
     override fun getNetwork(): Network {
         return Network.POLYGON
