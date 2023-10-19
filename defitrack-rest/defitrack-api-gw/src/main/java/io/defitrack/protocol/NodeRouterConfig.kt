b13dab6c63924f9558a397e918d5f9d612bc4f26
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-19 10:44:12 +0200

    node subroutes

--- defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/protocol/NodeRouterConfig.kt
+++ defitrack-rest/defitrack-api-gw/src/main/java/io/defitrack/protocol/NodeRouterConfig.kt
@@ -3,8 +3,9 @@ package io.defitrack.protocol
 import io.defitrack.protocol.distribution.Node
 import io.defitrack.protocol.distribution.ProtocolDistributionConfig
 import io.github.reactivecircus.cache4k.Cache
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
@@ -26,20 +27,41 @@ class NodeRouterConfig(
     val cache = Cache.Builder<String, List<Node>>().expireAfterWrite(6.hours).build()
 
     fun getNodes(serverRequest: ServerRequest): Mono<ServerResponse> = runBlocking {
-         ServerResponse.ok()
+        ServerResponse.ok()
             .contentType(MediaType.APPLICATION_JSON)
             .body(
                 BodyInserters.fromValue(
-                    cache.get("nodes") {
-                        distributionConfig.getConfigs()
-                    }
+                    fetchNodes()
                 )
             )
+    }
+
+    private suspend fun fetchNodes() = cache.get("nodes") {
+        distributionConfig.getConfigs()
     }
 
     @Bean
     fun nodesRoute(): RouterFunction<ServerResponse> {
         return RouterFunctions
             .route(GET("/nodes"), ::getNodes)
+    }
+
+    @Bean
+    fun nodesSubRoutes(builder: RouteLocatorBuilder): RouteLocator = runBlocking {
+        val routeBuilder = builder.routes()
+
+        fetchNodes().forEach { node ->
+            routeBuilder.route("node-" + node.name) {
+                it.path(true, "/${node.name}/**")
+                    .filters { filter ->
+                        filter.rewritePath(
+                            "/${node.name}/(?<segment>.*)",
+                            "/\${segment}"
+                        )
+                    }
+                    .uri("http://defitrack-group-${node.name}.default.svc.cluster.local:8080/api")
+            }
+        }
+        routeBuilder.build()
     }
 }
