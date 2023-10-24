27830ccc54d12ccece0b55e1fcd2e4e3a4e8ca09
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-24 11:36:56 +0200

    fix balancer base gauges

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/balancer/staking/BalancerBaseGaugeMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/balancer/staking/BalancerBaseGaugeMarketProvider.kt
@@ -1,17 +1,120 @@
 package io.defitrack.protocol.balancer.staking
 
+import io.defitrack.abi.TypeUtils
+import io.defitrack.claimable.domain.ClaimableRewardFetcher
+import io.defitrack.claimable.domain.Reward
+import io.defitrack.common.network.Network
+import io.defitrack.common.utils.BigDecimalExtensions.dividePrecisely
 import io.defitrack.conditional.ConditionalOnCompany
+import io.defitrack.erc20.TokenInformationVO
+import io.defitrack.event.EventDecoder.Companion.extract
+import io.defitrack.evm.GetEventLogsCommand
+import io.defitrack.market.farming.FarmingMarketProvider
+import io.defitrack.market.farming.domain.FarmingMarket
+import io.defitrack.market.position.Position
+import io.defitrack.market.position.PositionFetcher
 import io.defitrack.protocol.Company
+import io.defitrack.protocol.Protocol
+import io.defitrack.protocol.balancer.contract.BalancerGaugeZkEvmContract
 import io.defitrack.protocol.balancer.pooling.BalancerBasePoolingMarketProvider
 import io.defitrack.protocol.balancer.pooling.BalancerOptimismPoolingMarketProvider
+import io.defitrack.transaction.PreparedTransaction
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
+import org.web3j.abi.datatypes.Event
+import java.math.BigInteger
 
 @Component
 @ConditionalOnCompany(Company.BALANCER)
 @ConditionalOnProperty(value = ["base.enabled"], havingValue = "true", matchIfMissing = true)
-class BalancerBaseGaugeMarketProvider(
-    poolingMarketProvider: BalancerBasePoolingMarketProvider
-) : BalancerGaugeFarmingMarketProvider(
-    poolingMarketProvider, "0xb1a4FE1C6d25a0DDAb47431A92A723dd71d9021f"
-)
+class BalancerBaseGaugeMarketProvider() : FarmingMarketProvider() {
+
+    val factory = "0xb1a4FE1C6d25a0DDAb47431A92A723dd71d9021f"
+
+    val gaugeCreatedEvent = Event(
+        "GaugeCreated", listOf(TypeUtils.address(true))
+    )
+
+    override suspend fun fetchMarkets(): List<FarmingMarket> {
+        val logs = getBlockchainGateway().getEventsAsEthLog(
+            GetEventLogsCommand(
+                addresses = listOf(factory),
+                topic = "0xaa98436d09d130af48de49867af8b723bbbebb0d737638b5fe8f1bf31bbb71c0",
+                fromBlock = BigInteger.valueOf(203653L)
+            )
+        )
+        return logs.mapNotNull {
+            try {
+                val log = it.get()
+                val gaugeAddress: String = gaugeCreatedEvent.extract(log, true, 0)
+                val gaugecontract = BalancerGaugeZkEvmContract(
+                    getBlockchainGateway(),
+                    gaugeAddress
+                )
+
+                val lp = getToken(gaugecontract.lpToken.await())
+                val rewards = gaugecontract.rewardTokens().map { rewardToken ->
+                    getToken(rewardToken)
+                }
+
+                create(
+                    identifier = gaugeAddress,
+                    name = lp.name + " gauge",
+                    stakedToken = getToken(gaugecontract.getStakedToken()).toFungibleToken(),
+                    rewardTokens = rewards.map(TokenInformationVO::toFungibleToken),
+
+                    positionFetcher = PositionFetcher(
+                        gaugecontract.address,
+                        gaugecontract::workingBalance
+                    ) {
+                        val bal = it[0].value as BigInteger
+                        if (bal > BigInteger.ZERO) {
+                            val ratiod =
+                                bal.toBigDecimal().dividePrecisely(gaugecontract.workingSupply.await().toBigDecimal())
+                            val normalized = lp.totalSupply.toBigDecimal().times(ratiod)
+                            Position(
+                                bal,
+                                normalized.toBigInteger()
+                            )
+                        } else {
+                            Position(
+                                BigInteger.ZERO,
+                                BigInteger.ZERO
+                            )
+                        }
+                    },
+                    metadata = mapOf("address" to gaugeAddress),
+                    exitPositionPreparer = prepareExit {
+                        PreparedExit(
+                            function = gaugecontract.exitPosition(it.amount),
+                            to = gaugecontract.address,
+                        )
+                    },
+                    claimableRewardFetcher = ClaimableRewardFetcher(
+                        rewards = rewards.map {
+                            Reward(
+                                it.toFungibleToken(),
+                                gaugeAddress,
+                                { user -> gaugecontract.getClaimableRewardFunction(user, it.address) }
+                            )
+                        },
+                        preparedTransaction = PreparedTransaction.selfExecutingTransaction(gaugecontract::getClaimRewardsFunction)
+                    ),
+                )
+            } catch (ex: Exception) {
+                logger.error("Error fetching gauge", ex)
+                null
+            }
+        }
+    }
+
+
+    override fun getProtocol(): Protocol {
+        return Protocol.BALANCER
+    }
+
+    override fun getNetwork(): Network {
+        return Network.BASE
+    }
+
+}
