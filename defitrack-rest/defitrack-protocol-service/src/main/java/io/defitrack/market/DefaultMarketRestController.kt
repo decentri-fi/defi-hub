cd30563d8149dfb1264f233dfbd1c4e0b1d4320c
Quinten De Swaef <quintendeswaef@gmail.com>
2023-10-27 10:14:32 +0200

    add filter for proto

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/market/DefaultMarketRestController.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/market/DefaultMarketRestController.kt
@@ -1,11 +1,13 @@
 package io.defitrack.market
 
 import io.defitrack.PageUtils
+import io.defitrack.PageUtils.createPageFromList
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 
 abstract class DefaultMarketRestController<T : DefiMarket>(
     private val marketProviders: List<MarketProvider<T>>,
@@ -16,25 +18,31 @@ abstract class DefaultMarketRestController<T : DefiMarket>(
     @Operation(summary = "Get all markets for a specific protocol")
     fun allMarkets(
         @PathVariable("protocol") protocol: String,
+        @RequestParam("network", required = false) network: String? = null
     ): List<MarketVO> {
-        return getAllMarkets(protocol).map(marketMapper::map)
+        return getAllMarkets(protocol, network).map(marketMapper::map)
     }
 
     @GetMapping("/markets", params = ["paged"])
     fun pagedMarkets(
         @PathVariable("protocol") protocol: String,
+        @RequestParam("network", required = false) network: String? = null,
         pageable: Pageable
     ): Page<MarketVO> {
         val allMarkets = getAllMarkets(
-            protocol = protocol
+            protocol = protocol,
+            network = network
         )
 
-        return PageUtils.createPageFromList(allMarkets, pageable).map {
+        return createPageFromList(allMarkets, pageable).map {
             marketMapper.map(it)
         }
     }
 
-    private fun getAllMarkets(protocol: String) = marketProviders
+    private fun getAllMarkets(protocol: String, network: String? = null) = marketProviders
+        .filter {
+            network == null || it.getNetwork().slug == network || it.getNetwork().name == network
+        }
         .filter {
             it.getProtocol().slug == protocol
         }
