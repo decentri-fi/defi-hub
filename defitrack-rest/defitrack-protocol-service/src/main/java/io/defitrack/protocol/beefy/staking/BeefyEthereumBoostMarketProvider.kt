0dd0bb11b72bb167d05875ca49e544e5a41a69fe
Quinten De Swaef <quintendeswaef@gmail.com>
2023-11-13 14:55:55 +0100

    Beefy Mistakes were made

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/beefy/staking/BeefyEthereumBoostMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/beefy/staking/BeefyEthereumBoostMarketProvider.kt
@@ -1,11 +1,14 @@
 package io.defitrack.protocol.beefy.staking
 
 import io.defitrack.common.network.Network
+import io.defitrack.conditional.ConditionalOnCompany
+import io.defitrack.protocol.Company
 import io.defitrack.protocol.beefy.BeefyBoostService
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnCompany(Company.BEEFY)
 @ConditionalOnProperty(value = ["ethereum.enabled"], havingValue = "true", matchIfMissing = true)
 class BeefyEthereumBoostMarketProvider(
     beefyBoostService: BeefyBoostService,
