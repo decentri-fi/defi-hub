a7fcc425d51cf9f7afca35e9512c361fbda8a248
Quinten De Swaef <quintendeswaef@gmail.com>
2023-11-14 09:17:34 +0100

    simplify solidlizard concurrency

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/solidlizard/pooling/SolidLizardPoolingMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/solidlizard/pooling/SolidLizardPoolingMarketProvider.kt
@@ -1,5 +1,7 @@
 package io.defitrack.protocol.solidlizard.pooling
 
+import arrow.fx.coroutines.parMap
+import arrow.fx.coroutines.parMapNotNull
 import io.defitrack.common.network.Network
 import io.defitrack.common.utils.FormatUtilsExtensions.asEth
 import io.defitrack.common.utils.Refreshable.Companion.refreshable
@@ -18,40 +20,38 @@ import org.springframework.stereotype.Component
 @Component
 @ConditionalOnCompany(Company.SOLIDLIZARD)
 class SolidLizardPoolingMarketProvider : PoolingMarketProvider() {
-    override suspend fun fetchMarkets(): List<PoolingMarket> = coroutineScope {
+    override suspend fun fetchMarkets(): List<PoolingMarket> {
         val factory = PairFactoryContract(
             getBlockchainGateway(), "0x734d84631f00dc0d3fcd18b04b6cf42bfd407074"
         )
 
-        return@coroutineScope factory.allPairs().map {
-            async {
+        return factory.allPairs().parMapNotNull(concurrency = 12) {
 
-                val token = getToken(it)
-                val tokens = token.underlyingTokens.map(TokenInformationVO::toFungibleToken)
+            val token = getToken(it)
+            val tokens = token.underlyingTokens.map(TokenInformationVO::toFungibleToken)
 
-                try {
-                    val breakdown = fiftyFiftyBreakdown(tokens[0], tokens[1], token.address)
-                    create(
-                        name = token.name,
-                        identifier = token.address,
-                        marketSize = refreshable(breakdown.sumOf { it.reserveUSD }) {
-                            fiftyFiftyBreakdown(tokens[0], tokens[1], token.address).sumOf { it.reserveUSD }
-                        },
-                        positionFetcher = defaultPositionFetcher(token.address),
-                        tokens = token.underlyingTokens.map(TokenInformationVO::toFungibleToken),
-                        symbol = token.symbol,
-                        breakdown = breakdown,
-                        address = token.address,
-                        totalSupply = refreshable(token.totalSupply.asEth(token.decimals)) {
-                            getToken(it).totalSupply.asEth(token.decimals)
-                        }
-                    )
-                } catch (ex: Exception) {
-                    logger.error("Error while fetching pooling market $it", ex)
-                    null
-                }
+            try {
+                val breakdown = fiftyFiftyBreakdown(tokens[0], tokens[1], token.address)
+                create(
+                    name = token.name,
+                    identifier = token.address,
+                    marketSize = refreshable(breakdown.sumOf { it.reserveUSD }) {
+                        fiftyFiftyBreakdown(tokens[0], tokens[1], token.address).sumOf { it.reserveUSD }
+                    },
+                    positionFetcher = defaultPositionFetcher(token.address),
+                    tokens = token.underlyingTokens.map(TokenInformationVO::toFungibleToken),
+                    symbol = token.symbol,
+                    breakdown = breakdown,
+                    address = token.address,
+                    totalSupply = refreshable(token.totalSupply.asEth(token.decimals)) {
+                        getToken(it).totalSupply.asEth(token.decimals)
+                    }
+                )
+            } catch (ex: Exception) {
+                logger.error("Error while fetching pooling market $it", ex)
+                null
             }
-        }.awaitAll().filterNotNull()
+        }.filterNotNull()
     }
 
     override fun getProtocol(): Protocol {
