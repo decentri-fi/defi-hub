0a8f50be278c3254f70e0795d39a7d154bc098a4
Quinten De Swaef <quintendeswaef@gmail.com>
2023-11-20 16:44:51 +0100

    liquity and convex

--- defitrack-protocols/src/main/java/io/defitrack/protocol/convex/ConvexEthereumService.kt
+++ defitrack-protocols/src/main/java/io/defitrack/protocol/convex/ConvexEthereumService.kt
@@ -10,7 +10,8 @@ class ConvexEthereumService {
             "0x3Fe65692bfCD0e6CF84cB1E7d24108E434A7587e",
             "0xCF50b810E57Ac33B91dCF525C6ddd9881B139332",
             "0x0a760466e1b4621579a82a39cb56dda2f4e70f03",
-            "0x6b27d7bc63f1999d14ff9ba900069ee516669ee8"
+            "0x6b27d7bc63f1999d14ff9ba900069ee516669ee8",
+            "0x7e880867363a7e321f5d260cade2b0bb2f717b02"
         )
     }
 
---
+++ defitrack-protocols/src/main/java/io/defitrack/protocol/liquity/LiquityStakingContract.kt
@@ -0,0 +1,23 @@
+package io.defitrack.protocol.liquity
+
+import arrow.core.nonEmptyListOf
+import io.defitrack.abi.TypeUtils.Companion.toAddress
+import io.defitrack.abi.TypeUtils.Companion.uint256
+import io.defitrack.evm.contract.BlockchainGateway
+import io.defitrack.evm.contract.EvmContract
+import org.web3j.abi.datatypes.Function
+
+class LiquityStakingContract(
+    blockchainGateway: BlockchainGateway, address: String
+) : EvmContract(blockchainGateway, address) {
+
+
+    fun stakes(user: String): Function {
+        return createFunction(
+            "stakes",
+            nonEmptyListOf(user.toAddress()),
+            nonEmptyListOf(uint256())
+        )
+    }
+
+}
