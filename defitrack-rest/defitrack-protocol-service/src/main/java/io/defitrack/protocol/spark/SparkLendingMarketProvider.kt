183b043bfe15c0c17974e0e7f083e882071325ba
Quinten De Swaef <quintendeswaef@gmail.com>
2023-12-08 10:48:18 +0100

    upgrade to spring 3.2

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/spark/SparkLendingMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/spark/SparkLendingMarketProvider.kt
@@ -1,7 +1,7 @@
 package io.defitrack.protocol.spark
 
+import arrow.fx.coroutines.parMapNotNull
 import io.defitrack.common.network.Network
-import io.defitrack.common.utils.AsyncUtils.lazyAsync
 import io.defitrack.common.utils.FormatUtilsExtensions.asEth
 import io.defitrack.common.utils.Refreshable
 import io.defitrack.conditional.ConditionalOnCompany
@@ -10,54 +10,42 @@ import io.defitrack.market.lending.domain.LendingMarket
 import io.defitrack.market.position.PositionFetcher
 import io.defitrack.protocol.Company
 import io.defitrack.protocol.Protocol
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Component
 
 @Component
 @ConditionalOnCompany(Company.SPARK)
 class SparkLendingMarketProvider : LendingMarketProvider() {
 
-    val pool = lazyAsync {
-        io.defitrack.protocol.spark.PoolContract(
+    override suspend fun fetchMarkets(): List<LendingMarket> {
+
+        val poolContract = PoolContract(
             getBlockchainGateway(),
             "0xC13e21B648A5Ee794902342038FF3aDAB66BE987"
         )
-    }
 
-    override suspend fun fetchMarkets(): List<LendingMarket> {
-        val poolContract = pool.await()
-        return coroutineScope {
-            poolContract.reservesList().map {
-                async {
-                    throttled {
-                        try {
+        return poolContract.reservesList().parMapNotNull(concurrency = 8) {
+            try {
+                val reserveData = poolContract.getReserveData(it)
+                val aToken = getToken(reserveData.aTokenAddress)
+                val underlying = getToken(it)
 
-                            val reserveData = poolContract.getReserveData(it)
-                            val aToken = getToken(reserveData.aTokenAddress)
-                            val underlying = getToken(it)
-
-                            create(
-                                identifier = aToken.address,
-                                name = "Spark " + aToken.name,
-                                token = underlying.toFungibleToken(),
-                                poolType = "spark",
-                                positionFetcher = PositionFetcher(
-                                    aToken.asERC20Contract(getBlockchainGateway())::balanceOfFunction
-                                ),
-                                marketToken = aToken.toFungibleToken(),
-                                totalSupply = Refreshable.refreshable(aToken.totalSupply.asEth(aToken.decimals)) {
-                                    getToken(aToken.address).totalSupply.asEth(aToken.decimals)
-                                }
-                            )
-                        } catch (ex: Exception) {
-                            logger.error("Unable to fetch V3 Lending market with address $it", ex)
-                            null
-                        }
+                create(
+                    identifier = aToken.address,
+                    name = aToken.name,
+                    token = underlying,
+                    poolType = "spark",
+                    positionFetcher = PositionFetcher(
+                        aToken.asERC20Contract(getBlockchainGateway())::balanceOfFunction
+                    ),
+                    marketToken = aToken,
+                    totalSupply = Refreshable.refreshable(aToken.totalSupply.asEth(aToken.decimals)) {
+                        getToken(aToken.address).totalSupply.asEth(aToken.decimals)
                     }
-                }
-            }.awaitAll().filterNotNull()
+                )
+            } catch (ex: Exception) {
+                logger.error("Unable to fetch V3 Lending market with address $it", ex)
+                null
+            }
         }
     }
 
