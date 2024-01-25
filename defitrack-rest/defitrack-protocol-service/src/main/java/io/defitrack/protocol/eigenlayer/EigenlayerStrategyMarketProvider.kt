8a5c21ad8563ebccdeec78e77fe12e6254a1982d
Quinten De Swaef <quintendeswaef@gmail.com>
2024-01-25 08:44:05 +0100

    more eigenlayer strats

--- defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/eigenlayer/EigenlayerStrategyMarketProvider.kt
+++ defitrack-rest/defitrack-protocol-service/src/main/java/io/defitrack/protocol/eigenlayer/EigenlayerStrategyMarketProvider.kt
@@ -15,7 +15,15 @@ import org.springframework.stereotype.Component
 class EigenlayerStrategyMarketProvider : FarmingMarketProvider() {
 
     val strategies = listOf(
-        "0x93c4b944d05dfe6df7645a86cd2206016c51564d"
+        "0x93c4b944d05dfe6df7645a86cd2206016c51564d",
+        "0x0fe4f44bee93503346a3ac9ee5a26b130a5796d6",
+        "0x1bee69b7dfffa4e2d53c2a2df135c388ad25dcd2",
+        "0x9d7ed45ee2e8fc5482fa2428f15c971e6369011d",
+        "0x54945180db7943c0ed0fee7edab2bd24620256bc",
+        "0x57ba429517c3473b6d34ca9acd56c0e735b94c02",
+        "0x7ca911e83dabf90c90dd3de5411a10f1a6112184",
+        "0xa4c637e0f704745d182e4d38cab7e7485321d059",
+        "0x13760f50a9d7377e4f20cb8cf9e4c26586c658ff"
     )
 
     override suspend fun fetchMarkets(): List<FarmingMarket> {
