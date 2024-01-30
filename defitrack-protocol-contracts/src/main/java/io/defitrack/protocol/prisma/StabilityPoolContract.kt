468e361ad1d29471e3a10c4136a158a357f0e9d5
Quinten De Swaef <quintendeswaef@gmail.com>
2024-01-30 15:15:24 +0100

    prisma stability pool

---
+++ defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/prisma/StabilityPoolContract.kt
@@ -0,0 +1,38 @@
+package io.defitrack.protocol.prisma
+
+import arrow.core.nel
+import io.defitrack.abi.TypeUtils.Companion.toAddress
+import io.defitrack.abi.TypeUtils.Companion.toUint256
+import io.defitrack.abi.TypeUtils.Companion.uint256
+import io.defitrack.evm.contract.BlockchainGateway
+import io.defitrack.evm.contract.ContractCall
+import io.defitrack.evm.contract.EvmContract
+import java.math.BigInteger
+
+class StabilityPoolContract(
+    blockchainGateway: BlockchainGateway, address: String
+) : EvmContract(blockchainGateway, address) {
+
+    fun claimableReward(user: String): ContractCall {
+        return createFunction(
+            "claimableReward",
+            user.toAddress().nel(),
+            uint256().nel()
+        )
+    }
+
+    fun accountDeposits(user: String): ContractCall {
+        return createFunction(
+            "accountDeposits",
+            user.toAddress().nel(),
+            uint256().nel()
+        )
+    }
+
+    fun claimFn(): ContractCall {
+        return createFunction(
+            "withdrawFromSP",
+            listOf(BigInteger.ZERO.toUint256())
+        )
+    }
+}
