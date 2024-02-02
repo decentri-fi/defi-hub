92d87f6ce3143b54b5b76a02926aeeb276474ee8
Quinten De Swaef <quintendeswaef@gmail.com>
2024-02-02 13:47:03 +0100

    stader liquid positions

---
+++ defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/stader/MaticXStakingContract.kt
@@ -0,0 +1,21 @@
+package io.defitrack.protocol.stader
+
+import arrow.core.nel
+import io.defitrack.abi.TypeUtils.Companion.toUint256
+import io.defitrack.abi.TypeUtils.Companion.uint256
+import io.defitrack.evm.contract.BlockchainGateway
+import io.defitrack.evm.contract.EvmContract
+import java.math.BigInteger
+
+class MaticXStakingContract(
+    blockchainGateway: BlockchainGateway, address: String
+) : EvmContract(blockchainGateway, address) {
+
+    suspend fun convertMaticXToMatic(amount: BigInteger) : BigInteger {
+        return readSingle(
+            "convertMaticXToMatic",
+            amount.toUint256().nel(),
+            uint256()
+        )
+    }
+}
