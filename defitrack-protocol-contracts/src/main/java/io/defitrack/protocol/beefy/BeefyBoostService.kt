e8a944775243fb06ffd7f3ac2c7df4d5ef4592b4
Quinten De Swaef <quintendeswaef@gmail.com>
2024-02-08 13:21:38 +0100

    don't load beefy boosts if we don't need em

--- defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/beefy/BeefyBoostService.kt
+++ defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/beefy/BeefyBoostService.kt
@@ -3,12 +3,15 @@ package io.defitrack.protocol.beefy
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.defitrack.common.network.Network
+import io.defitrack.common.utils.AsyncUtils.lazyAsync
 import io.defitrack.protocol.beefy.domain.BeefyLaunchPool
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import jakarta.annotation.PostConstruct
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,7 +19,6 @@ class BeefyBoostService(
     private val objectMapper: ObjectMapper,
     private val client: HttpClient
 ) {
-
 
     val beefyMapping = mapOf(
         Network.POLYGON to "polygon",
@@ -27,24 +29,26 @@ class BeefyBoostService(
         Network.BASE to "base"
     )
 
-    private val beefyBoosts: MutableMap<Network, List<BeefyLaunchPool>> = mutableMapOf()
-
-    @PostConstruct
-    fun startup() {
-        runBlocking {
-            val vaultsAsList: String =
-                client.get("https://api.beefy.finance/boosts").bodyAsText()
-            val vaults = objectMapper.readValue(vaultsAsList, object : TypeReference<List<BeefyLaunchPool>>() {})
-
-            beefyMapping.map { (network, chain) ->
-                beefyBoosts[network] = vaults.filter {
-                    it.chain == chain
-                }
-            }
-        }
+    private val beefyBoosts = lazyAsync {
+        load()
     }
 
-    fun getBoosts(network: Network): List<BeefyLaunchPool> {
-        return beefyBoosts.getOrDefault(network, emptyList())
+    suspend fun load(): MutableMap<Network, List<BeefyLaunchPool>> = withContext(Dispatchers.IO) {
+        val retVal: MutableMap<Network, List<BeefyLaunchPool>> = mutableMapOf()
+
+        val vaultsAsList: String =
+            client.get("https://api.beefy.finance/boosts").bodyAsText()
+        val vaults = objectMapper.readValue(vaultsAsList, object : TypeReference<List<BeefyLaunchPool>>() {})
+
+        beefyMapping.forEach { (network, chain) ->
+            retVal[network] = vaults.filter {
+                it.chain == chain
+            }
+        }
+        retVal
+    }
+
+    suspend fun getBoosts(network: Network): List<BeefyLaunchPool> {
+        return beefyBoosts.await().getOrDefault(network, emptyList())
     }
 }
