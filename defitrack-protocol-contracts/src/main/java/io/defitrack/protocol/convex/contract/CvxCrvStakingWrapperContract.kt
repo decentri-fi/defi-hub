4f2d8644169968d611b7b86d90c83e79d1853a7a
Quinten De Swaef <quintendeswaef@gmail.com>
2024-03-03 18:41:38 -0700

    fix crv contract

--- defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/convex/contract/CvxCrvStakingWrapperContract.kt
+++ defitrack-protocol-contracts/src/main/java/io/defitrack/protocol/convex/contract/CvxCrvStakingWrapperContract.kt
@@ -2,6 +2,7 @@ package io.defitrack.protocol.convex.contract
 
 import arrow.core.nel
 import io.defitrack.abi.TypeUtils
+import io.defitrack.abi.TypeUtils.Companion.address
 import io.defitrack.abi.TypeUtils.Companion.toAddress
 import io.defitrack.common.utils.AsyncUtils.lazyAsync
 import io.defitrack.evm.contract.BlockchainGateway
@@ -12,19 +13,13 @@ import kotlinx.coroutines.Deferred
 context(BlockchainGateway)
 class CvxCrvStakingWrapperContract( address: String) : ERC20Contract(address) {
 
-    //todo: fix
-    val crv: Deferred<String> = lazyAsync {
-        readSingle("crv", TypeUtils.address())
-    }
+    val crv = constant<String>("crv", address())
+    val cvxCrv = constant<String>("cvxCrv", address())
 
     fun getRewardFn(user: String): ContractCall {
         return createFunction(
             "getReward",
             user.toAddress().nel(),
         )
-    }
-
-    val cvxCrv: Deferred<String> = lazyAsync {
-        readSingle("cvxCrv", TypeUtils.address())
     }
 }
