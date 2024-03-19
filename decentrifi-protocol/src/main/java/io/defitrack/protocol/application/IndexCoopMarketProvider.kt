d64ce4f469ac6b601927b89a77bbdbc0a79a0d90
Quinten De Swaef <quintendeswaef@gmail.com>
2024-03-19 16:32:37 +0100

    indexcoop ethx2

---
+++ decentrifi-protocol/src/main/java/io/defitrack/protocol/application/IndexCoopMarketProvider.kt
@@ -0,0 +1,61 @@
+package io.defitrack.protocol.application
+
+import arrow.core.nel
+import io.defitrack.common.network.Network
+import io.defitrack.common.utils.FormatUtilsExtensions.asEth
+import io.defitrack.common.utils.refreshable
+import io.defitrack.market.domain.PoolingMarket
+import io.defitrack.market.domain.PoolingMarketTokenShare
+import io.defitrack.market.domain.farming.FarmingMarket
+import io.defitrack.market.port.out.FarmingMarketProvider
+import io.defitrack.market.port.out.PoolingMarketProvider
+import io.defitrack.protocol.Protocol
+import io.defitrack.protocol.set.SetTokenContract
+import org.springframework.stereotype.Component
+
+@Component
+class IndexCoopMarketProvider : PoolingMarketProvider() {
+
+    val ethx2 = "0x65c4c0517025ec0843c9146af266a2c5a2d148a2"
+
+    override suspend fun fetchMarkets(): List<PoolingMarket> {
+        val tokenContract = with(getBlockchainGateway()) { SetTokenContract(ethx2) }
+        val token = getToken(ethx2)
+
+        val breakdown = refreshable {
+            tokenContract.getPositions().map {
+                val supply = tokenContract.totalSupply().get().asEth(tokenContract.readDecimals())
+                val underlying = getToken(it.token)
+                val reserve = it.amount.toBigDecimal()
+                    .times(tokenContract.getPositionMultiplier().asEth())
+                    .times(supply).toBigInteger()
+                PoolingMarketTokenShare(
+                    token = underlying,
+                    reserve = reserve,
+                )
+            }
+        }
+        return create(
+            identifier = ethx2,
+            address = ethx2,
+            name = token.name,
+            symbol = token.symbol,
+            tokens = breakdown.get().map { element -> element.token },
+            breakdown = breakdown,
+            apr = null,
+            positionFetcher = defaultPositionFetcher(ethx2),
+            investmentPreparer = null,
+            totalSupply = refreshable(token.totalDecimalSupply()) {
+                getToken(ethx2).totalDecimalSupply()
+            }
+        ).nel()
+    }
+
+    override fun getProtocol(): Protocol {
+        return Protocol.SET
+    }
+
+    override fun getNetwork(): Network {
+        return Network.ETHEREUM
+    }
+}
