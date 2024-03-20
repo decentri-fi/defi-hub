700ac1dca18a16de28dbcd965e01a7439bb655ed
Quinten De Swaef <quintendeswaef@gmail.com>
2024-03-20 14:58:33 +0100

    coingecko rework

--- defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/application/AggregatedPriceProvider.kt
+++ defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/application/AggregatedPriceProvider.kt
@@ -1,9 +1,7 @@
 package io.defitrack.price.application
 
 import io.defitrack.erc20.domain.FungibleTokenInformation
-import io.defitrack.price.external.adapter.coingecko.CoinGeckoPriceService
 import io.github.reactivecircus.cache4k.Cache
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 import kotlin.time.Duration.Companion.minutes
@@ -11,24 +9,14 @@ import kotlin.time.Duration.Companion.minutes
 @Component
 class AggregatedPriceProvider(
     private val priceAggregator: PriceAggregator,
-    private val coingeckoSerivce: CoinGeckoPriceService
 ) {
-
-    private val logger = LoggerFactory.getLogger(this::class.java)
 
     val priceCache = Cache.Builder<String, BigDecimal>().expireAfterWrite(5.minutes).build()
 
     suspend fun getPrice(token: FungibleTokenInformation): BigDecimal {
         return priceCache.get("${token.address.lowercase()}-${token.network.toNetwork().slug}") {
             priceAggregator.getPrice(token.address, token.network.name.lowercase())?.price
-                ?: fromCoingecko(token)
                 ?: BigDecimal.ZERO
         }
-    }
-
-    private suspend fun fromCoingecko(token: FungibleTokenInformation): BigDecimal? {
-        return coingeckoSerivce.getPrice(token.address)?.also {
-            logger.debug("getting price on coingecko for ${token.name} (${token.symbol}) on ${token.network.name}")
-        } ?: BigDecimal.ZERO
     }
 }
--- defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/external/adapter/coingecko/CoinGeckoPriceService.kt
+++ defitrack-rest/defitrack-price/src/main/java/io/defitrack/price/external/adapter/coingecko/CoinGeckoPriceService.kt
@@ -1,30 +1,45 @@
 package io.defitrack.price.external.adapter.coingecko
 
 import arrow.core.Either
+import arrow.core.Either.Companion.catch
 import arrow.core.getOrElse
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.michaelbull.retry.policy.binaryExponentialBackoff
+import com.github.michaelbull.retry.policy.limitAttempts
+import com.github.michaelbull.retry.policy.plus
+import com.github.michaelbull.retry.retry
 import com.google.gson.JsonParser
+import io.defitrack.common.network.Network
+import io.defitrack.price.external.domain.ExternalPrice
+import io.defitrack.price.port.out.ExternalPriceService
 import io.github.reactivecircus.cache4k.Cache
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.nio.charset.Charset
 
 @Service
+@ConditionalOnProperty("oracles.coingecko.enabled", havingValue = "true", matchIfMissing = true)
 class CoinGeckoPriceService(
     private val httpClient: HttpClient,
-    private val objectMapper: ObjectMapper
-) {
+    private val objectMapper: ObjectMapper,
+    @Value("\${coingecko.api-key:test}") private val apiKey: String
+) : ExternalPriceService {
 
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
     val coinlistLocation = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
@@ -48,7 +63,7 @@ class CoinGeckoPriceService(
     suspend fun getCoingeckoTokens(): Set<CoingeckoToken> {
         return tokenCache.get("all") {
             logger.info("fetching all coingecko tokens")
-            Either.catch {
+            catch {
                 val result = withContext(Dispatchers.IO) {
                     httpClient.get(coinlistLocation).bodyAsText(Charset.defaultCharset())
                 }
@@ -59,32 +74,53 @@ class CoinGeckoPriceService(
         }
     }
 
-    suspend fun getTokenByAddress(address: String): CoingeckoToken? {
-        return getCoingeckoTokens().firstOrNull { token ->
-            token.platforms.entries.any {
-                it.value != null && (it.value.lowercase() == address.lowercase())
+    private fun fromNetwork(network: String): Network? {
+        return Network.fromString(network)
+    }
+
+    override suspend fun getAllPrices(): Flow<ExternalPrice> = channelFlow {
+        val tokens = getCoingeckoTokens()
+        tokens.chunked(400).map { chunk ->
+
+            val ids = chunk.joinToString(",") { it.id }
+
+            val response = getPrices(ids).bodyAsText()
+            val jsonObject = JsonParser.parseString(response)
+            chunk.map { token ->
+                if (!jsonObject.asJsonObject.has(token.id) || !jsonObject.asJsonObject[token.id].asJsonObject.has("usd")) {
+                    logger.debug("price for ${token.id} not found: {}", jsonObject.asJsonObject[token.id].toString())
+                    emptyList()
+                } else {
+                    val price = jsonObject.asJsonObject[token.id].asJsonObject["usd"].asBigDecimal
+                    token.platforms.entries.mapNotNull { (network, address) ->
+                        fromNetwork(network)?.let {
+                            ExternalPrice(address, it, price, "coingecko", "coingecko", 100)
+                        }
+                    }
+                }
+            }.flatten()
+        }.flatten().forEach {
+            send(it)
+        }
+    }
+
+    suspend fun getPrices(ids: String): HttpResponse {
+        delay(1000)
+        return retry(limitAttempts(10) + binaryExponentialBackoff(1000, 10000)) {
+            try {
+                val response =
+                    httpClient.get("https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd&x_cg_demo_api_key=${apiKey}") { }
+                if (response.status.value == 429) {
+                    logger.info("problem fetching prices, retrying: {}", response.bodyAsText())
+                    throw ThrottledException()
+                }
+                response
+            } catch (ex: Exception) {
+                logger.debug("problem trying to get prices from coingecko", ex)
+                throw ex
             }
         }
     }
 
-    suspend fun getPrice(address: String): BigDecimal? = withContext(Dispatchers.IO) {
-        try {
-            if (address.isBlank()) {
-                BigDecimal.ZERO
-            } else {
-                getTokenByAddress(address)?.let { token ->
-                    val response: String =
-                        httpClient.get("https://api.coingecko.com/api/v3/simple/price?ids=${token.id}&vs_currencies=usd")
-                            .bodyAsText()
-                    val jsonObject = JsonParser.parseString(response)
-                    jsonObject.asJsonObject[token.id].asJsonObject["usd"].asBigDecimal.also {
-                        logger.info("had to get ${token.name} on coingecko: {}", it)
-                    }
-                }
-            }
-        } catch (ex: Exception) {
-            logger.debug("error trying to fetch price for $address", ex)
-            null
-        }
-    }
+    class ThrottledException : RuntimeException()
 }
