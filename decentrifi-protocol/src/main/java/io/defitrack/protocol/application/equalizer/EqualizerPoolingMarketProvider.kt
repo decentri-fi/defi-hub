4fa7d460d92fc5bf0661f3c1a677cddf4185c44f
Quinten De Swaef <quintendeswaef@gmail.com>
2024-03-27 17:01:31 +0100

    equalizer rework

--- decentrifi-protocol/src/main/java/io/defitrack/protocol/application/equalizer/EqualizerPoolingMarketProvider.kt
+++ decentrifi-protocol/src/main/java/io/defitrack/protocol/application/equalizer/EqualizerPoolingMarketProvider.kt
@@ -1,17 +1,13 @@
 package io.defitrack.protocol.application.equalizer
 
-import arrow.core.Either
-import arrow.fx.coroutines.parMapNotNull
-import io.defitrack.common.network.Network
-import io.defitrack.common.utils.refreshable
 import io.defitrack.architecture.conditional.ConditionalOnCompany
-import io.defitrack.market.port.out.PoolingMarketProvider
+import io.defitrack.common.network.Network
 import io.defitrack.market.domain.PoolingMarket
+import io.defitrack.market.port.out.PoolingMarketProvider
 import io.defitrack.protocol.Company
 import io.defitrack.protocol.Protocol
 import io.defitrack.protocol.equalizer.EqualizerVoter
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
 import org.springframework.stereotype.Component
 
 @ConditionalOnCompany(Company.EQUALIZER)
@@ -20,45 +16,11 @@ class EqualizerPoolingMarketProvider : PoolingMarketProvider() {
 
     private val voterAddress = "0x46abb88ae1f2a35ea559925d99fdc5441b592687"
 
-    override suspend fun produceMarkets(): Flow<PoolingMarket> = channelFlow {
+    override suspend fun produceMarkets(): Flow<PoolingMarket> {
         val contract = EqualizerVoter(
             getBlockchainGateway(), voterAddress
         )
-
-        contract.pools().parMapNotNull(concurrency = 12) { pool ->
-            Either.catch {
-                val lp = getToken(pool)
-
-                if (lp.underlyingTokens.isEmpty()) {
-                    throw IllegalArgumentException("Underlying tokens are empty for $pool")
-                }
-
-                val breakdown = refreshable {
-                    breakdownOf(
-                        lp.address,
-                        lp.underlyingTokens[0],
-                        lp.underlyingTokens[1],
-                    )
-                }
-
-                create(
-                    name = lp.name,
-                    identifier = lp.address,
-                    address = lp.address,
-                    positionFetcher = defaultPositionFetcher(lp.address),
-                    breakdown = breakdown,
-                    erc20Compatible = true,
-                    symbol = lp.symbol,
-                    totalSupply = refreshable {
-                        getToken(lp.address).totalDecimalSupply()
-                    }
-                )
-            }.mapLeft {
-                logger.error("Error while creating pooling market {}: {} ", pool, it.message)
-            }.getOrNull()
-        }.forEach {
-            send(it)
-        }
+        return contract.pools().pairsToMarkets()
     }
 
     override fun getProtocol(): Protocol {
